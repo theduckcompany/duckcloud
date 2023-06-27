6750fa4f00235095d7f709826ab98aca1c825009
Peltoche <pierre.peltier@protonmail.com>
2023-06-27 14:40:45 +0200

    Create a basic server with an hello handler

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/Peltoche/neurone
+
+go 1.20
+
+require (
+	go.uber.org/fx v1.20.0
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/dig v1.17.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
---
+++ pkg/tools/httprouter/http_router.go
@@ -0,0 +1,39 @@
+package httprouter
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/Peltoche/neurone/pkg/tools/logger"
+	"go.uber.org/fx"
+)
+
+// MuxHandler is an http.Handler that knows the mux pattern
+// under which it will be registered.
+type MuxHandler interface {
+	Register(*http.ServeMux)
+
+	// Strings reports the handler name
+	String() string
+}
+
+func NewServer(lc fx.Lifecycle, mux *http.ServeMux, log *logger.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			log.Info(fmt.Sprintf("Starting HTTP server at %s", srv.Addr))
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
