000832e0286fde68660cf166a817c2b103b32ca9
Peltoche <pierre.peltier@protonmail.com>
2023-06-28 11:27:52 +0200

    Move the logger to the json format

--- src/tools/logger/init.go
+++ src/tools/logger/init.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewSLogger() *slog.Logger {
-	return slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+	return slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
 		// Remove default time slog.Attr. It will be replaced by the one
 		// from the router middleware.
 		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
