fae4b6ab75eeac662c87518f63875ee091b37505
Peltoche <pierre.peltier@protonmail.com>
2023-06-29 12:15:15 +0200

    Add the debug and dev flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/Peltoche/neurone/src/server"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Peltoche/neurone/src/server"
+	"golang.org/x/exp/slog"
+)
 
 func main() {
 	cfg := server.NewDefaultConfig()
 
-	server.Start(cfg)
+	var debug bool
+
+	flag.BoolVar(&cfg.Dev, "dev", false, "Run in dev mode and make json prettier")
+	flag.BoolVar(&debug, "debug", false, "Force the debug level")
+
+	flag.Parse()
+
+	if debug {
+		cfg.Log.Level = slog.LevelDebug
+	}
+
+	err := server.Start(cfg)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
