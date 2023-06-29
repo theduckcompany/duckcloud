9da9ce604f8f7dde213339db8fa6b05f0b2ad48d
Peltoche <pierre.peltier@protonmail.com>
2023-06-29 12:15:15 +0200

    Add the debug and dev flags

--- src/tools/logger/init.go
+++ src/tools/logger/init.go
@@ -6,8 +6,13 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-func NewSLogger() *slog.Logger {
+type Config struct {
+	Level slog.Level `mapstructure:"level"`
+}
+
+func NewSLogger(cfg Config) *slog.Logger {
 	return slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level: cfg.Level,
 		// Remove default time slog.Attr. It will be replaced by the one
 		// from the router middleware.
 		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
