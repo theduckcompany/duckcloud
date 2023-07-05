69bfba37ea2ac64e6cf6b2adae0547408c67eb0b
Peltoche <pierre.peltier@protonmail.com>
2023-07-05 15:59:03 +0200

    Move back all the oauth2 logic into the oauth2 package

--- src/service/oauth2/http_handler.go
+++ src/service/oauth2/http_handler.go
@@ -1,13 +1,16 @@
 package oauth2
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-oauth2/oauth2/v4"
-	"github.com/go-oauth2/oauth2/v4/errors"
+	oerrors "github.com/go-oauth2/oauth2/v4/errors"
 	"github.com/go-oauth2/oauth2/v4/manage"
 	"github.com/go-oauth2/oauth2/v4/server"
 	"github.com/go-session/session"
@@ -18,6 +21,7 @@ import (
 	"github.com/Peltoche/neurone/src/service/oauthsessions"
 	"github.com/Peltoche/neurone/src/service/users"
 	"github.com/Peltoche/neurone/src/tools"
+	"github.com/Peltoche/neurone/src/tools/errs"
 	"github.com/Peltoche/neurone/src/tools/jwt"
 	"github.com/Peltoche/neurone/src/tools/response"
 	"github.com/Peltoche/neurone/src/tools/router"
@@ -31,30 +35,31 @@ const (
 // HTTPHandler handle all the oauth2 requests.
 type HTTPHandler struct {
 	logger   *slog.Logger
-	uuid     uuid.Service
 	jwt      jwt.Parser
-	users    users.Service
-	client   oauthclients.Service
 	response response.Writer
-	srv      *server.Server
-	session  oauthsessions.Service
+	uuid     uuid.Service
+
+	srv     *server.Server
+	manager *manage.Manager
+
+	users   users.Service
+	clients oauthclients.Service
+	code    oauthcodes.Service
+	session oauthsessions.Service
 }
 
 // NewHTTPHandler setup a new Oauth2Server.
 func NewHTTPHandler(
 	tools tools.Tools,
 	users users.Service,
-	client oauthclients.Service,
+	clients oauthclients.Service,
 	code oauthcodes.Service,
-	session oauthsessions.Service) *HTTPHandler {
+	session oauthsessions.Service,
+) *HTTPHandler {
 	manager := manage.NewDefaultManager()
 	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
-
-	uuid := tools.UUID()
-
-	manager.MapTokenStorage(&tokenStorage{uuid, code, session})
-	manager.MapClientStorage(&clientStorage{client: client})
-
+	manager.MapTokenStorage(&tokenStorage{tools.UUID(), code, session})
+	manager.MapClientStorage(&clientStorage{client: clients})
 	manager.MapAccessGenerate(tools.JWT().GenerateAccess())
 
 	srv := server.NewServer(&server.Config{
@@ -69,22 +74,26 @@ func NewHTTPHandler(
 			oauth2.CodeChallengeS256,
 		},
 	}, manager)
-	srv.SetClientInfoHandler(server.ClientFormHandler)
 
 	res := &HTTPHandler{
-		uuid:     uuid,
 		logger:   tools.Logger(),
 		response: tools.ResWriter(),
 		jwt:      tools.JWT(),
-		users:    users,
-		client:   client,
-		srv:      srv,
-		session:  session,
+		uuid:     tools.UUID(),
+
+		srv:     srv,
+		manager: manager,
+
+		users:   users,
+		clients: clients,
+		code:    code,
+		session: session,
 	}
 
 	srv.SetInternalErrorHandler(res.errorHandler)
 	srv.SetResponseErrorHandler(res.responseErrorHandler)
 	srv.SetUserAuthorizationHandler(res.userAuthorizationHandler)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
 
 	return res
 }
@@ -93,6 +102,13 @@ func NewHTTPHandler(
 func (h *HTTPHandler) Register(r chi.Router, mids router.Middlewares) {
 	auth := r.With(mids.StripSlashed, mids.Logger)
 
+	// Web pages
+	auth.HandleFunc("/login", h.handleLoginPage)
+	auth.HandleFunc("/login", h.handleLoginPage)
+	auth.HandleFunc("/forgot", h.handleForgotPage)
+	auth.HandleFunc("/consent", h.handleConsentPage)
+
+	// Actions
 	auth.Post("/auth/logout", h.handleLogoutEndpoint)
 	auth.HandleFunc("/auth/authorize", h.handleAuthorizationEndpoint)
 	auth.HandleFunc("/auth/token", h.handleTokenEndpoint)
@@ -136,7 +152,7 @@ func (h *HTTPHandler) handleLogoutEndpoint(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	err = h.session.RemoveByAccessToken(r.Context(), token.Raw)
+	err = h.manager.RemoveAccessToken(r.Context(), token.Raw)
 	if err != nil {
 		h.response.WriteJSONError(w, err)
 		return
@@ -177,13 +193,165 @@ func (h *HTTPHandler) handleAuthorizationEndpoint(w http.ResponseWriter, r *http
 	}
 }
 
-func (h *HTTPHandler) responseErrorHandler(res *errors.Response) {
+func (h *HTTPHandler) handleForgotPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		h.response.WriteHTML(w, http.StatusOK, "auth/forgot.html", nil)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("Not implemented yet!"))
+}
+
+func (h *HTTPHandler) handleLoginPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		h.response.WriteHTML(w, http.StatusOK, "auth/login.html", nil)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		h.response.WriteJSONError(w, errs.BadRequest(err, "invalid form url"))
+		return
+	}
+
+	inputs := map[string]string{}
+	loginErrors := map[string]string{}
+
+	inputs["username"] = r.Form.Get("username")
+
+	user, err := h.users.Authenticate(r.Context(), r.Form.Get("username"), r.Form.Get("password"))
+	var status int
+	switch {
+	case err == nil:
+		// continue
+	case errors.Is(err, users.ErrInvalidUsername):
+		loginErrors["username"] = "User doesn't exists"
+		status = http.StatusBadRequest
+	case errors.Is(err, users.ErrInvalidPassword):
+		loginErrors["password"] = "Invalid password"
+		status = http.StatusBadRequest
+	default:
+		loginErrors["notif"] = "Unexpected error"
+		status = http.StatusBadRequest
+	}
+
+	if len(loginErrors) > 0 {
+		h.response.WriteHTML(w, status, "auth/login.html", map[string]interface{}{
+			"inputs": inputs,
+			"errors": loginErrors,
+		})
+		return
+	}
+
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, ok := store.Get("ReturnUri")
+	if !ok {
+		// There is not session created yet. This append when a user land directly on
+		// /login page without calling the /auth/authorize first (which would have
+		// redirect him to the /login page).
+		// In that case we assume that the user want to connect to the web app and so we
+		// will create the session with the correct client.
+		client, err := h.clients.GetByID(r.Context(), oauthclients.WebAppClientID)
+		if err != nil {
+			h.response.WriteJSONError(w, err)
+			return
+		}
+
+		if client == nil {
+			h.response.WriteJSONError(w, err)
+			return
+		}
+
+		sessionID := string(h.uuid.New())
+		form := url.Values{}
+		form.Add("client_id", string(client.ID))
+		form.Add("response_type", "code")
+		form.Add("redirect_uri", client.RedirectURI)
+		form.Add("user_id", string(user.ID))
+		form.Add("session_id", sessionID)
+		form.Add("scope", client.Scopes.String())
+		form.Add("state", string(h.uuid.New()))
+
+		r.Form = form
+		store.Set("ReturnUri", r.Form)
+	}
+
+	store.Set("LoggedInUserID", user.ID)
+	store.Save()
+
+	r.ParseForm()
+
+	if ok, _ := strconv.ParseBool(r.Form.Get("skipValidation")); ok {
+		w.Header().Set("Location", "/auth/authorize")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	w.Header().Set("Location", "/consent")
+	w.WriteHeader(http.StatusFound)
+}
+
+func (h *HTTPHandler) handleConsentPage(w http.ResponseWriter, r *http.Request) {
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		h.response.WriteJSONError(w, err)
+		return
+	}
+
+	session, ok := store.Get("ReturnUri")
+	if !ok {
+		// This is not session created yet. This append whan a user land directy on
+		// the /consent without calling /login first.
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	form := session.(url.Values)
+
+	userID, ok := store.Get("LoggedInUserID")
+	if !ok {
+		h.response.WriteJSON(w, http.StatusBadRequest, errors.New("user not authenticated"))
+		return
+	}
+
+	user, err := h.users.GetByID(r.Context(), userID.(uuid.UUID))
+	if err != nil || user == nil {
+		h.response.WriteJSON(w, http.StatusBadRequest, fmt.Errorf("failed to find the user %q: %w", userID, err))
+		return
+	}
+
+	clientID := form.Get("client_id")
+	client, err := h.clients.GetByID(r.Context(), clientID)
+	if err != nil {
+		h.response.WriteJSON(w, http.StatusBadRequest, fmt.Errorf("failed to find the client %q: %w", clientID, err))
+		return
+	}
+
+	if client == nil {
+		h.response.WriteJSON(w, http.StatusBadRequest, fmt.Errorf("client %q doesn't exists", clientID))
+		return
+	}
+
+	h.response.WriteHTML(w, http.StatusOK, "auth/consent.html", map[string]interface{}{
+		"clientName": client.Name,
+		"username":   user.Username,
+		"scope":      strings.Split(form.Get("scope"), ","),
+	})
+}
+
+func (h *HTTPHandler) responseErrorHandler(res *oerrors.Response) {
 	h.logger.Error("OAUTH2 response error", slog.String("error", res.Error.Error()), slog.String("description", res.Description))
 }
 
-func (h *HTTPHandler) errorHandler(err error) *errors.Response {
+func (h *HTTPHandler) errorHandler(err error) *oerrors.Response {
 	h.logger.Error("OAUTH2 internal Error", slog.String("error", err.Error()))
-	return errors.NewResponse(fmt.Errorf("internal error"), http.StatusInternalServerError)
+	return oerrors.NewResponse(fmt.Errorf("internal error"), http.StatusInternalServerError)
 }
 
 type debugLogger struct {
