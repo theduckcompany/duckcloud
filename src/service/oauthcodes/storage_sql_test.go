9a25211beba3f6b3b392a8ac0c706ff5849afcda
Peltoche <pierre.peltier@protonmail.com>
2023-07-08 09:48:26 +0200

    Add some test to the oauthcodes sql storage

--- src/service/oauthcodes/storage_sql_test.go
+++ src/service/oauthcodes/storage_sql_test.go
@@ -7,31 +7,18 @@ import (
 
 	"github.com/Peltoche/neurone/src/tools"
 	"github.com/Peltoche/neurone/src/tools/storage"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
-type StorageTestSuite struct {
-	suite.Suite
-	storage  *sqlStorage
-	nowData  time.Time
-	codeData Code
-}
+func TestCodeStorage(t *testing.T) {
+	ctx := context.Background()
+	now := time.Now().UTC()
 
-func TestCodeStorageSuite(t *testing.T) {
-	suite.Run(t, new(StorageTestSuite))
-}
-
-func (suite *StorageTestSuite) SetupSuite() {
-	t := suite.T()
-	tools := tools.NewMock(t)
-
-	suite.nowData = time.Now().UTC()
-
-	suite.codeData = Code{
+	codeExample := Code{
 		Code:            "some-code",
-		CreatedAt:       suite.nowData,
-		ExpiresAt:       suite.nowData.Add(time.Hour),
+		CreatedAt:       now,
+		ExpiresAt:       now.Add(time.Hour),
 		ClientID:        "some-client-id",
 		UserID:          "some-user-id",
 		RedirectURI:     "http://some-redirect.com/uri",
@@ -40,14 +27,42 @@ func (suite *StorageTestSuite) SetupSuite() {
 		ChallengeMethod: "plain",
 	}
 
+	tools := tools.NewMock(t)
+
 	db, err := storage.NewSQliteDBWithMigrate(storage.Config{Path: t.TempDir() + "/test.db"}, tools)
 	require.NoError(t, err)
 
-	suite.storage = newSqlStorage(db)
-}
+	storage := newSqlStorage(db)
 
-func (suite *StorageTestSuite) Test_Save() {
-	err := suite.storage.Save(context.Background(), &suite.codeData)
+	t.Run("save", func(t *testing.T) {
+		err := storage.Save(context.Background(), &codeExample)
+		assert.NoError(t, err)
+	})
 
-	suite.Assert().NoError(err)
+	t.Run("GetByID ok", func(t *testing.T) {
+		code, err := storage.GetByCode(context.Background(), "some-code")
+		assert.NoError(t, err)
+		assert.EqualValues(t, &codeExample, code)
+	})
+
+	t.Run("GetByID not found", func(t *testing.T) {
+		code, err := storage.GetByCode(ctx, "some-invalid-code")
+		assert.NoError(t, err)
+		assert.Nil(t, code)
+	})
+
+	t.Run("RemoveByCode", func(t *testing.T) {
+		err := storage.RemoveByCode(ctx, "some-code")
+		assert.NoError(t, err)
+
+		// Check that the code is no more available
+		code, err := storage.GetByCode(ctx, "some-code")
+		assert.NoError(t, err)
+		assert.Nil(t, code)
+	})
+
+	t.Run("RemoveByCode invalid code", func(t *testing.T) {
+		err := storage.RemoveByCode(ctx, "some-invalid-code")
+		assert.NoError(t, err)
+	})
 }
