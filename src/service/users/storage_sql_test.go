b68c131c85c7ce2f68c799d8ea1d6b3c6cb057f6
Pierre Peltier <pierre.peltier@protonmail.com>
2023-07-18 14:05:14 +0200

    Remove all the test suites (#19)

--- src/service/users/storage_sql_test.go
+++ src/service/users/storage_sql_test.go
@@ -8,94 +8,81 @@ import (
 	"github.com/Peltoche/neurone/src/tools"
 	"github.com/Peltoche/neurone/src/tools/storage"
 	"github.com/Peltoche/neurone/src/tools/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
-type SqlStorageTestSuite struct {
-	suite.Suite
-	storage  *sqlStorage
-	nowData  time.Time
-	userData User
-}
-
-func TestUserSqlStorageSuite(t *testing.T) {
-	suite.Run(t, new(SqlStorageTestSuite))
-}
-
-func (suite *SqlStorageTestSuite) SetupSuite() {
-	t := suite.T()
+func TestUserSqlStorage(t *testing.T) {
+	nowData := time.Now().UTC()
 	tools := tools.NewMock(t)
 
-	suite.nowData = time.Now().UTC()
-
-	suite.userData = User{
+	userData := User{
 		ID:        uuid.UUID("some-uuid"),
 		Username:  "some-username",
 		Email:     "some-email",
 		password:  "some-password",
-		CreatedAt: suite.nowData,
+		CreatedAt: nowData,
 	}
 
 	db, err := storage.NewSQliteDBWithMigrate(storage.Config{Path: t.TempDir() + "/test.db"}, tools)
 	require.NoError(t, err)
 
-	suite.storage = newSqlStorage(db)
-}
+	storage := newSqlStorage(db)
 
-func (suite *SqlStorageTestSuite) Test_Create() {
-	err := suite.storage.Save(context.Background(), &suite.userData)
+	t.Run("Create success", func(t *testing.T) {
+		err := storage.Save(context.Background(), &userData)
 
-	suite.Assert().NoError(err)
-}
+		assert.NoError(t, err)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByID() {
-	res, err := suite.storage.GetByID(context.Background(), "some-uuid")
+	t.Run("GetByID success", func(t *testing.T) {
+		res, err := storage.GetByID(context.Background(), "some-uuid")
 
-	suite.Require().NotNil(res)
-	res.CreatedAt = res.CreatedAt.UTC()
+		assert.NotNil(t, res)
+		res.CreatedAt = res.CreatedAt.UTC()
 
-	suite.NoError(err)
-	suite.Equal(&suite.userData, res)
-}
+		assert.NoError(t, err)
+		assert.Equal(t, &userData, res)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByID_invalid_return_nil() {
-	res, err := suite.storage.GetByID(context.Background(), "some-invalid-id")
+	t.Run("GetByID not found", func(t *testing.T) {
+		res, err := storage.GetByID(context.Background(), "some-invalid-id")
 
-	suite.NoError(err)
-	suite.Nil(res)
-}
+		assert.NoError(t, err)
+		assert.Nil(t, res)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByEmail() {
-	res, err := suite.storage.GetByEmail(context.Background(), "some-email")
+	t.Run("GetByEmail success", func(t *testing.T) {
+		res, err := storage.GetByEmail(context.Background(), "some-email")
 
-	suite.Require().NotNil(res)
-	res.CreatedAt = res.CreatedAt.UTC()
+		assert.NotNil(t, res)
+		res.CreatedAt = res.CreatedAt.UTC()
 
-	suite.NoError(err)
-	suite.Equal(&suite.userData, res)
-}
+		assert.NoError(t, err)
+		assert.Equal(t, &userData, res)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByEmail_invalid_return_nil() {
-	res, err := suite.storage.GetByEmail(context.Background(), "some-invalid-email")
+	t.Run("GetByEmail not found", func(t *testing.T) {
+		res, err := storage.GetByEmail(context.Background(), "some-invalid-email")
 
-	suite.NoError(err)
-	suite.Nil(res)
-}
+		assert.NoError(t, err)
+		assert.Nil(t, res)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByUsername() {
-	res, err := suite.storage.GetByUsername(context.Background(), "some-username")
+	t.Run("GetByUsername success", func(t *testing.T) {
+		res, err := storage.GetByUsername(context.Background(), "some-username")
 
-	suite.Require().NotNil(res)
-	res.CreatedAt = res.CreatedAt.UTC()
+		assert.NotNil(t, res)
+		res.CreatedAt = res.CreatedAt.UTC()
 
-	suite.Assert().NoError(err)
-	suite.Assert().Equal(&suite.userData, res)
-}
+		assert.NoError(t, err)
+		assert.Equal(t, &userData, res)
+	})
 
-func (suite *SqlStorageTestSuite) Test_GetByUsername_invalid_return_nil() {
-	res, err := suite.storage.GetByUsername(context.Background(), "some-invalid-username")
+	t.Run("GetByUsername not found", func(t *testing.T) {
+		res, err := storage.GetByUsername(context.Background(), "some-invalid-username")
 
-	suite.NoError(err)
-	suite.Nil(res)
+		assert.NoError(t, err)
+		assert.Nil(t, res)
+	})
 }
