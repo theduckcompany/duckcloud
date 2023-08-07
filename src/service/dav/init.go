37c8fcf36f15528ce7c4e383c96b39d4047128e2
Peltoche <pierre.peltier@protonmail.com>
2023-08-07 19:05:13 +0200

    Add a basic dav auth

--- src/service/dav/init.go
+++ src/service/dav/init.go
@@ -1,11 +1,12 @@
 package dav
 
 import (
+	"github.com/Peltoche/neurone/src/service/inodes"
 	"golang.org/x/net/webdav"
 )
 
 type Service = webdav.FileSystem
 
-func Init() Service {
-	return NewFSService()
+func Init(inodes inodes.Service) Service {
+	return NewFSService(inodes)
 }
