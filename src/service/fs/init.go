1733e7d7a691d15e1c61acec752785182dd123ed
Peltoche <pierre.peltier@protonmail.com>
2023-08-10 08:07:35 +0200

    Create the fs service

---
+++ src/service/fs/init.go
@@ -0,0 +1,14 @@
+package fs
+
+import (
+	"context"
+	"os"
+)
+
+type Service interface {
+	Mkdir(ctx context.Context, name string, perm os.FileMode) error
+	OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (File, error)
+	RemoveAll(ctx context.Context, name string) error
+	Rename(ctx context.Context, oldName, newName string) error
+	Stat(ctx context.Context, name string) (os.FileInfo, error)
+}
