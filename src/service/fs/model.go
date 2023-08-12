714ef8d955415a079237c0a0d43453ce9cbdbb1e
Peltoche <pierre.peltier@protonmail.com>
2023-08-12 11:04:58 +0200

    Add the inodes part for the file creation

--- src/service/fs/model.go
+++ src/service/fs/model.go
@@ -1,11 +1,49 @@
 package fs
 
 import (
-	"io"
-	"net/http"
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+
+	"github.com/Peltoche/neurone/src/service/inodes"
+	"github.com/Peltoche/neurone/src/tools/storage"
 )
 
-type File interface {
-	http.File
-	io.Writer
+type File struct {
+	inode    *inodes.INode
+	inodeSvc inodes.Service
+	cmd      *inodes.PathCmd
+}
+
+func (d *File) Close() error                                 { return nil }
+func (d *File) Read(p []byte) (int, error)                   { return 0, fs.ErrInvalid }
+func (d *File) Write(p []byte) (int, error)                  { return 0, fs.ErrInvalid }
+func (d *File) Seek(offset int64, whence int) (int64, error) { return 0, fs.ErrInvalid }
+
+func (d *File) Readdir(count int) ([]fs.FileInfo, error) {
+	if d.inode.Mode().IsDir() {
+		return nil, fs.ErrInvalid
+	}
+
+	// TODO: Check if we should use the context from `OpenFile`
+	res, err := d.inodeSvc.Readdir(context.Background(), d.cmd, &storage.PaginateCmd{
+		StartAfter: map[string]string{"name": ""},
+		Limit:      count,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to Readdir: %w", err)
+	}
+
+	var infos []fs.FileInfo
+
+	for idx := range res {
+		infos = append(infos, &res[idx])
+	}
+
+	return infos, nil
+}
+
+func (d *File) Stat() (os.FileInfo, error) {
+	return d.inode, nil
 }
