baef88122f65a059428cdd4ca66cc124ca54b049
Pierre Peltier <pierre.peltier@protonmail.com>
2023-08-21 13:08:00 +0400

    Add the dav authentification (#110)

--- src/service/dav/service.go
+++ src/service/dav/service.go
@@ -7,13 +7,8 @@ import (
 	"github.com/theduckcompany/duckcloud/src/service/files"
 	"github.com/theduckcompany/duckcloud/src/service/fs"
 	"github.com/theduckcompany/duckcloud/src/service/inodes"
-	"github.com/theduckcompany/duckcloud/src/tools/uuid"
+	"github.com/theduckcompany/duckcloud/src/service/users"
 	"golang.org/x/net/webdav"
-)
-
-const (
-	currentUser = uuid.UUID("a6e4082a-124f-403b-92b4-de3253a908a4")
-	root        = uuid.UUID("cdd75a48-c4e4-468f-be3c-b171c028e281")
 )
 
 type davFS struct {
@@ -22,24 +17,31 @@ type davFS struct {
 }
 
 func (s *davFS) Mkdir(ctx context.Context, name string, perm os.FileMode) error {
-	// username := ctx.Value(usernameKeyCtx)
-	// password := ctx.Value(passwordKeyCtx)
+	user := ctx.Value(userKeyCtx).(*users.User)
 
-	return fs.NewFSService(currentUser, root, s.inodes, s.files).CreateDir(ctx, name, perm)
+	return fs.NewFSService(user.ID(), user.RootFS(), s.inodes, s.files).CreateDir(ctx, name, perm)
 }
 
 func (s *davFS) OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (webdav.File, error) {
-	return fs.NewFSService(currentUser, root, s.inodes, s.files).OpenFile(ctx, name, flag, perm)
+	user := ctx.Value(userKeyCtx).(*users.User)
+
+	return fs.NewFSService(user.ID(), user.RootFS(), s.inodes, s.files).OpenFile(ctx, name, flag, perm)
 }
 
 func (s *davFS) RemoveAll(ctx context.Context, name string) error {
-	return fs.NewFSService(currentUser, root, s.inodes, s.files).RemoveAll(ctx, name)
+	user := ctx.Value(userKeyCtx).(*users.User)
+
+	return fs.NewFSService(user.ID(), user.RootFS(), s.inodes, s.files).RemoveAll(ctx, name)
 }
 
 func (s *davFS) Rename(ctx context.Context, oldName, newName string) error {
+	// user := ctx.Value(userKeyCtx).(*users.User)
+
 	return webdav.ErrNotImplemented
 }
 
 func (s *davFS) Stat(ctx context.Context, name string) (os.FileInfo, error) {
-	return fs.NewFSService(currentUser, root, s.inodes, s.files).Stat(ctx, name)
+	user := ctx.Value(userKeyCtx).(*users.User)
+
+	return fs.NewFSService(user.ID(), user.RootFS(), s.inodes, s.files).Stat(ctx, name)
 }
