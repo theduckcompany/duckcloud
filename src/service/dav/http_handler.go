28f3cd1e0490f9b9ce9ce8a45b4feb85ed90cef5
Peltoche <pierre.peltier@protonmail.com>
2023-08-21 10:35:15 +0200

    Add the dav authentification

--- src/service/dav/http_handler.go
+++ src/service/dav/http_handler.go
@@ -2,12 +2,14 @@ package dav
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/theduckcompany/duckcloud/src/service/dav/internal"
 	"github.com/theduckcompany/duckcloud/src/service/files"
 	"github.com/theduckcompany/duckcloud/src/service/inodes"
+	"github.com/theduckcompany/duckcloud/src/service/users"
 	"github.com/theduckcompany/duckcloud/src/tools"
 	"github.com/theduckcompany/duckcloud/src/tools/router"
 	"golang.org/x/net/webdav"
@@ -15,19 +17,18 @@ import (
 
 type davKeyCtx string
 
-var (
-	usernameKeyCtx davKeyCtx = "username"
-	passwordKeyCtx davKeyCtx = "password"
-)
+var userKeyCtx davKeyCtx = "user"
 
 // HTTPHandler serve files via the Webdav protocol over http.
 type HTTPHandler struct {
+	users      users.Service
 	davHandler *webdav.Handler
 }
 
 // NewHTTPHandler builds a new EchoHandler.
-func NewHTTPHandler(tools tools.Tools, inodes inodes.Service, files files.Service) *HTTPHandler {
+func NewHTTPHandler(tools tools.Tools, inodes inodes.Service, files files.Service, users users.Service) *HTTPHandler {
 	return &HTTPHandler{
+		users: users,
 		davHandler: &webdav.Handler{
 			Prefix:     "/dav",
 			FileSystem: &davFS{inodes, files},
@@ -53,10 +54,17 @@ func (h *HTTPHandler) handle(w http.ResponseWriter, r *http.Request) {
 	if !ok {
 		w.Header().Add("WWW-Authenticate", `Basic realm="fs"`)
 		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
-	ctx := context.WithValue(r.Context(), usernameKeyCtx, username)
-	ctx = context.WithValue(ctx, passwordKeyCtx, password)
+	user, err := h.users.Authenticate(r.Context(), username, password)
+	if errors.Is(err, users.ErrInvalidUsername) || errors.Is(err, users.ErrInvalidPassword) {
+		w.Header().Add("WWW-Authenticate", `Basic realm="fs"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	ctx := context.WithValue(r.Context(), userKeyCtx, user)
 
 	h.davHandler.ServeHTTP(w, r.WithContext(ctx))
 }
