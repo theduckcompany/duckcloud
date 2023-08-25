ec47586480ed242e3f3ebdff3bfdba134bceae71
Pierre Peltier <pierre.peltier@protonmail.com>
2023-08-25 13:20:04 +0400

    Do not authorize the last admin deletion (#141)

--- src/service/users/service_test.go
+++ src/service/users/service_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/theduckcompany/duckcloud/src/service/inodes"
@@ -12,21 +11,10 @@ import (
 	"github.com/theduckcompany/duckcloud/src/tools/errs"
 	"github.com/theduckcompany/duckcloud/src/tools/password"
 	"github.com/theduckcompany/duckcloud/src/tools/storage"
-	"github.com/theduckcompany/duckcloud/src/tools/uuid"
 )
 
 func Test_Users_Service(t *testing.T) {
 	ctx := context.Background()
-
-	now := time.Now()
-	user := User{
-		id:        uuid.UUID("some-user-id"),
-		username:  "some-username",
-		isAdmin:   true,
-		fsRoot:    uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f"),
-		password:  "some-encrypted-password",
-		createdAt: now,
-	}
 
 	t.Run("Create success", func(t *testing.T) {
 		tools := tools.NewMock(t)
@@ -34,24 +22,24 @@ func Test_Users_Service(t *testing.T) {
 		inodesSvc := inodes.NewMockService(t)
 		service := NewService(tools, store, inodesSvc)
 
-		store.On("GetByUsername", ctx, "some-username").Return(nil, nil).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(nil, nil).Once()
 
-		tools.UUIDMock.On("New").Return(uuid.UUID("some-user-id")).Once()
+		tools.UUIDMock.On("New").Return(ExampleAlice.ID()).Once()
 
-		inodesSvc.On("BootstrapUser", ctx, uuid.UUID("some-user-id")).Return(&inodes.ExampleAliceRoot, nil).Once()
+		inodesSvc.On("BootstrapUser", ctx, ExampleAlice.ID()).Return(&inodes.ExampleAliceRoot, nil).Once()
 
 		tools.ClockMock.On("Now").Return(now).Once()
-		tools.PasswordMock.On("Encrypt", ctx, "some-password").Return("some-encrypted-password", nil).Once()
+		tools.PasswordMock.On("Encrypt", ctx, "some-password").Return(ExampleAlice.password, nil).Once()
 
-		store.On("Save", ctx, &user).Return(nil)
+		store.On("Save", ctx, &ExampleAlice).Return(nil)
 
 		res, err := service.Create(ctx, &CreateCmd{
-			Username: "some-username",
+			Username: ExampleAlice.Username(),
 			Password: "some-password",
 			IsAdmin:  true,
 		})
 		assert.NoError(t, err)
-		assert.Equal(t, &user, res)
+		assert.Equal(t, &ExampleAlice, res)
 	})
 
 	t.Run("Create with a taken username", func(t *testing.T) {
@@ -60,11 +48,11 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(&User{}, nil).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(&User{}, nil).Once()
 
 		res, err := service.Create(ctx, &CreateCmd{
-			Username: "some-username",
-			Password: "some-password",
+			Username: ExampleAlice.Username(),
+			Password: ExampleAlice.password,
 			IsAdmin:  false,
 		})
 		assert.ErrorIs(t, err, ErrUsernameTaken)
@@ -78,11 +66,11 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(nil, fmt.Errorf("some-error")).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(nil, fmt.Errorf("some-error")).Once()
 
 		res, err := service.Create(ctx, &CreateCmd{
-			Username: "some-username",
-			Password: "some-password",
+			Username: ExampleAlice.Username(),
+			Password: ExampleAlice.password,
 			IsAdmin:  false,
 		})
 		assert.ErrorContains(t, err, "some-error")
@@ -95,13 +83,13 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(&user, nil).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(&ExampleAlice, nil).Once()
 
-		tools.PasswordMock.On("Compare", ctx, "some-encrypted-password", "some-password").Return(nil).Once()
+		tools.PasswordMock.On("Compare", ctx, ExampleAlice.password, "some-password").Return(nil).Once()
 
-		res, err := service.Authenticate(ctx, "some-username", "some-password")
+		res, err := service.Authenticate(ctx, ExampleAlice.Username(), "some-password")
 		assert.NoError(t, err)
-		assert.Equal(t, &user, res)
+		assert.Equal(t, &ExampleAlice, res)
 	})
 
 	t.Run("Authenticate with an invalid username", func(t *testing.T) {
@@ -110,9 +98,9 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(nil, nil).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(nil, nil).Once()
 
-		res, err := service.Authenticate(ctx, "some-username", "some-password")
+		res, err := service.Authenticate(ctx, ExampleAlice.Username(), ExampleAlice.password)
 		assert.ErrorIs(t, err, ErrInvalidUsername)
 		assert.Nil(t, res)
 	})
@@ -123,11 +111,11 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(&user, nil).Once()
-		tools.PasswordMock.On("Compare", ctx, "some-encrypted-password", "some-password").Return(password.ErrMissmatchedPassword).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(&ExampleAlice, nil).Once()
+		tools.PasswordMock.On("Compare", ctx, ExampleAlice.password, "some-invalid-password").Return(password.ErrMissmatchedPassword).Once()
 
 		// Invalid password here
-		res, err := service.Authenticate(ctx, "some-username", "some-password")
+		res, err := service.Authenticate(ctx, ExampleAlice.Username(), "some-invalid-password")
 		assert.ErrorIs(t, err, ErrInvalidPassword)
 		assert.Nil(t, res)
 	})
@@ -138,10 +126,10 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByUsername", ctx, "some-username").Return(&user, nil).Once()
-		tools.PasswordMock.On("Compare", ctx, "some-encrypted-password", "some-password").Return(fmt.Errorf("some-error")).Once()
+		store.On("GetByUsername", ctx, ExampleAlice.Username()).Return(&ExampleAlice, nil).Once()
+		tools.PasswordMock.On("Compare", ctx, ExampleAlice.password, "some-password").Return(fmt.Errorf("some-error")).Once()
 
-		res, err := service.Authenticate(ctx, "some-username", "some-password")
+		res, err := service.Authenticate(ctx, ExampleAlice.Username(), "some-password")
 		assert.EqualError(t, err, "failed to compare the hash and the password: some-error")
 		assert.Nil(t, res)
 	})
@@ -152,11 +140,11 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetByID", ctx, user.ID()).Return(&user, nil).Once()
+		store.On("GetByID", ctx, ExampleAlice.ID()).Return(&ExampleAlice, nil).Once()
 
-		res, err := service.GetByID(ctx, user.ID())
+		res, err := service.GetByID(ctx, ExampleAlice.ID())
 		assert.NoError(t, err)
-		assert.Equal(t, &user, res)
+		assert.Equal(t, &ExampleAlice, res)
 	})
 
 	t.Run("GetAll success", func(t *testing.T) {
@@ -165,10 +153,52 @@ func Test_Users_Service(t *testing.T) {
 		inodes := inodes.NewMockService(t)
 		service := NewService(tools, store, inodes)
 
-		store.On("GetAll", ctx, &storage.PaginateCmd{Limit: 10}).Return([]User{user}, nil).Once()
+		store.On("GetAll", ctx, &storage.PaginateCmd{Limit: 10}).Return([]User{ExampleAlice}, nil).Once()
 
 		res, err := service.GetAll(ctx, &storage.PaginateCmd{Limit: 10})
 		assert.NoError(t, err)
-		assert.Equal(t, []User{user}, res)
+		assert.Equal(t, []User{ExampleAlice}, res)
+	})
+
+	t.Run("Delete success", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		store := NewMockStorage(t)
+		inodes := inodes.NewMockService(t)
+		service := NewService(tools, store, inodes)
+
+		anAnotherAdmin := ExampleBob
+		anAnotherAdmin.isAdmin = true
+
+		store.On("GetByID", ctx, ExampleAlice.ID()).Return(&ExampleAlice, nil).Once()
+		store.On("GetAll", ctx, &storage.PaginateCmd{}).Return([]User{ExampleAlice, anAnotherAdmin}, nil).Once()
+		store.On("Delete", ctx, ExampleAlice.ID()).Return(nil).Once()
+
+		err := service.Delete(ctx, ExampleAlice.ID())
+		assert.NoError(t, err)
+	})
+
+	t.Run("Delete with a user not found", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		store := NewMockStorage(t)
+		inodes := inodes.NewMockService(t)
+		service := NewService(tools, store, inodes)
+
+		store.On("GetByID", ctx, ExampleAlice.ID()).Return(nil, nil).Once()
+
+		err := service.Delete(ctx, ExampleAlice.ID())
+		assert.NoError(t, err)
+	})
+
+	t.Run("Delete the last admin failed", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		store := NewMockStorage(t)
+		inodes := inodes.NewMockService(t)
+		service := NewService(tools, store, inodes)
+
+		store.On("GetByID", ctx, ExampleAlice.ID()).Return(&ExampleAlice, nil).Once()
+		store.On("GetAll", ctx, &storage.PaginateCmd{}).Return([]User{ExampleAlice}, nil).Once() // This is the last admin
+
+		err := service.Delete(ctx, ExampleAlice.ID())
+		assert.EqualError(t, err, "unauthorized: can't remove the last admin")
 	})
 }
