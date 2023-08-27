cb8f1d4ad91d47c14d8b3bf1d7eeeb45d49bf134
Peltoche <pierre.peltier@protonmail.com>
2023-08-27 16:22:59 +0200

    Add some test for the toolbox

---
+++ src/tools/mock_test.go
@@ -0,0 +1,23 @@
+package tools
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/theduckcompany/duckcloud/src/tools/clock"
+	"github.com/theduckcompany/duckcloud/src/tools/password"
+	"github.com/theduckcompany/duckcloud/src/tools/response"
+	"github.com/theduckcompany/duckcloud/src/tools/uuid"
+)
+
+func TestMockToolbox(t *testing.T) {
+	tools := NewMock(t)
+
+	assert.IsType(t, new(clock.MockClock), tools.Clock())
+	assert.IsType(t, new(uuid.MockService), tools.UUID())
+	assert.IsType(t, new(response.Default), tools.ResWriter())
+
+	assert.IsType(t, new(slog.Logger), tools.Logger())
+	assert.IsType(t, new(password.MockPassword), tools.Password())
+}
