d230579d95e88dedb79c5cd9f0e45074f261b37e
Pierre Peltier <pierre.peltier@protonmail.com>
2023-08-29 17:39:48 +0400

    Add the NewToolboxForTest function (#167)

--- src/tools/default_test.go
+++ src/tools/default_test.go
@@ -20,3 +20,13 @@ func TestDefaultToolbox(t *testing.T) {
 	assert.IsType(t, new(slog.Logger), tools.Logger())
 	assert.IsType(t, new(password.BcryptPassword), tools.Password())
 }
+
+func TestToolboxForTest(t *testing.T) {
+	tools := NewToolboxForTest(t)
+
+	assert.IsType(t, new(clock.Default), tools.Clock())
+	assert.IsType(t, new(uuid.Default), tools.UUID())
+	assert.IsType(t, new(response.Default), tools.ResWriter())
+	assert.IsType(t, new(slog.Logger), tools.Logger())
+	assert.IsType(t, new(password.BcryptPassword), tools.Password())
+}
