1266c4f943c04878fcd08f3da18a53d4d92e1aa9
Pierre Peltier <pierre.peltier@protonmail.com>
2023-09-04 17:20:07 +0400

    Create a basic home page (#194)

---
+++ src/web/home.go
@@ -0,0 +1,75 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/theduckcompany/duckcloud/src/service/users"
+	"github.com/theduckcompany/duckcloud/src/service/websessions"
+	"github.com/theduckcompany/duckcloud/src/tools"
+	"github.com/theduckcompany/duckcloud/src/tools/response"
+	"github.com/theduckcompany/duckcloud/src/tools/router"
+)
+
+type homeHandler struct {
+	response    response.Writer
+	webSessions websessions.Service
+	users       users.Service
+}
+
+func newHomeHandler(
+	tools tools.Tools,
+	webSessions websessions.Service,
+	users users.Service,
+) *homeHandler {
+	return &homeHandler{
+		response:    tools.ResWriter(),
+		webSessions: webSessions,
+		users:       users,
+	}
+}
+
+func (h *homeHandler) Register(r chi.Router, mids router.Middlewares) {
+	home := r.With(mids.RealIP, mids.StripSlashed, mids.Logger)
+
+	home.Get("/", h.getHome)
+}
+
+func (h *homeHandler) String() string {
+	return "web.home"
+}
+
+func (h *homeHandler) getHome(w http.ResponseWriter, r *http.Request) {
+	user, _ := h.getUserAndSession(w, r)
+	if user == nil {
+		return
+	}
+
+	fullPage := r.Header.Get("HX-Boosted") == "" && r.Header.Get("HX-Request") == ""
+	h.response.WriteHTML(w, http.StatusOK, "home/home.tmpl", fullPage, map[string]interface{}{})
+}
+
+func (h *homeHandler) getUserAndSession(w http.ResponseWriter, r *http.Request) (*users.User, *websessions.Session) {
+	ctx := r.Context()
+
+	currentSession, err := h.webSessions.GetFromReq(r)
+	if err != nil || currentSession == nil {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return nil, nil
+	}
+
+	user, err := h.users.GetByID(ctx, currentSession.UserID())
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf(`<div class="alert alert-danger role="alert">%s</div>`, err)))
+		return nil, nil
+	}
+
+	if user == nil {
+		_ = h.webSessions.Logout(r, w)
+		return nil, nil
+	}
+
+	return user, currentSession
+}
