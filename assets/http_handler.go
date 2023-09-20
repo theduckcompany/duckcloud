800bc97f57b08ed79b5b1ea915e5b354fc198b61
Peltoche <pierre.peltier@protonmail.com>
2023-09-20 12:11:22 +0200

    Refactor the asset handler to support cache and mime

--- assets/http_handler.go
+++ assets/http_handler.go
@@ -2,7 +2,12 @@ package assets
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
+	"os"
+	"path"
+	"strings"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/theduckcompany/duckcloud/src/tools/router"
@@ -16,30 +21,61 @@ type Config struct {
 }
 
 type HTTPHandler struct {
-	cfg Config
+	cfg       Config
+	assetFS   http.FileSystem
+	startDate time.Time
 }
 
 func NewHTTPHandler(cfg Config) *HTTPHandler {
-	return &HTTPHandler{cfg}
+	var assetFS http.FileSystem
+
+	switch cfg.HotReload {
+	case true:
+		assetFS = http.FS(os.DirFS("./assets/public"))
+	case false:
+		memFS, _ := fs.Sub(staticsFS, "public")
+		assetFS = http.FS(memFS)
+	}
+
+	return &HTTPHandler{cfg, assetFS, time.Now()}
 }
 
 // Register the http endpoints into the given mux server.
 func (h *HTTPHandler) Register(r chi.Router, _ *router.Middlewares) {
-	var server http.Handler
-
-	switch h.cfg.HotReload {
-	case true:
-		fs := http.Dir("./assets/public")
-		server = http.StripPrefix("/assets", http.FileServer(fs))
-	case false:
-		fs := http.FS(staticsFS)
-		server = http.FileServer(fs)
-	}
-
-	// No logger, no compression
-	r.Get("/assets/*", http.HandlerFunc(server.ServeHTTP))
+	r.Get("/assets/*", h.handleAsset)
 }
 
 func (h *HTTPHandler) String() string {
 	return "assets"
+}
+
+func (h *HTTPHandler) handleAsset(w http.ResponseWriter, r *http.Request) {
+	assetPath := strings.TrimPrefix(r.URL.Path, "/assets")
+	_, fileName := path.Split(assetPath)
+
+	f, err := h.assetFS.Open(assetPath)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var lastModified time.Time
+	if h.cfg.HotReload {
+		// For the cache validation in dev mod
+		w.Header().Add("Cache-Control", "no-cache")
+		fileInfo, err := f.Stat()
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		lastModified = fileInfo.ModTime()
+	} else {
+		// Expires header is for HTTP/1.1 and Cache-Controle is for the newer HTTP versions.
+		w.Header().Add("Expires", time.Now().Add(365*24*time.Hour).UTC().Format(http.TimeFormat))
+		w.Header().Add("Cache-Control", "max-age=31536000")
+		lastModified = h.startDate
+	}
+
+	http.ServeContent(w, r, fileName, lastModified, f)
 }
