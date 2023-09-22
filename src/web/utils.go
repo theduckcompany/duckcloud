4b169b8b455d81f7bf82da5486bb951cfba4a302
Peltoche <pierre.peltier@protonmail.com>
2023-09-22 15:28:05 +0200

    Handle the case where the web session doesn't exist for web

--- src/web/utils.go
+++ src/web/utils.go
@@ -30,10 +30,10 @@ func NewAuthenticator(webSessions websessions.Service, users users.Service, html
 func (a *Authenticator) getUserAndSession(w http.ResponseWriter, r *http.Request, access AccessType) (*users.User, *websessions.Session, bool) {
 	currentSession, err := a.webSessions.GetFromReq(r)
 	if errors.Is(err, websessions.ErrSessionNotFound) {
-		fmt.Printf("logout\n\n")
 		a.webSessions.Logout(r, w)
 		return nil, nil, true
 	}
+
 	if err != nil {
 		a.html.WriteHTMLErrorPage(w, r, fmt.Errorf("failed to websessions.GetFromReq: %w", err))
 		return nil, nil, true
