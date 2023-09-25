fe9460b0007b5e6faf5d2d5337bd077e828dc220
Pierre Peltier <pierre.peltier@protonmail.com>
2023-09-25 16:41:04 +0200

    Add the fs.ReadDir method (#265)

--- internal/service/fs/model.go
+++ internal/service/fs/model.go
@@ -7,11 +7,13 @@ import (
 	"os"
 
 	"github.com/theduckcompany/duckcloud/internal/service/inodes"
+	"github.com/theduckcompany/duckcloud/internal/tools/storage"
 )
 
 //go:generate mockery --name FS
 type FS interface {
 	CreateDir(ctx context.Context, name string) (*inodes.INode, error)
+	ReadDir(ctx context.Context, name string, cmd *storage.PaginateCmd) ([]inodes.INode, error)
 	OpenFile(ctx context.Context, name string, flag int) (FileOrDirectory, error)
 	RemoveAll(ctx context.Context, name string) error
 	Rename(ctx context.Context, oldName, newName string) error
