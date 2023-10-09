a7ab2b0179009de31fa28f36a8b4d35dfcda2683
Peltoche <pierre.peltier@protonmail.com>
2023-10-09 10:04:41 +0200

    Add the buildinfos package

---
+++ internal/tools/buildinfos/buildinfos.go
@@ -0,0 +1,9 @@
+package buildinfos
+
+var (
+	// Version of the release or "unknown"
+	Version string = "unknown"
+	// BuildTime is ISO-8601 UTC string representation of the time of
+	// the build or "unknown"
+	BuildTime string = "unknown"
+)
