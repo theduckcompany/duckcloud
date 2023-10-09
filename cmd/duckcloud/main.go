2f7edc3c1b28e68c3c2a996b5b47af6e4dc31678
Pierre Peltier <pierre.peltier@protonmail.com>
2023-10-09 10:17:07 +0200

    Add the buildinfos package (#307)

--- cmd/duckcloud/main.go
+++ cmd/duckcloud/main.go
@@ -6,14 +6,16 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/theduckcompany/duckcloud/cmd/duckcloud/commands"
 	"github.com/theduckcompany/duckcloud/cmd/duckcloud/commands/bootstrap"
+	"github.com/theduckcompany/duckcloud/internal/tools/buildinfos"
 )
 
 const binaryName = "duckcloud"
 
 func main() {
 	cmd := &cobra.Command{
-		Use:   binaryName,
-		Short: "Manage your duckcloud instance in your terminal.",
+		Use:     binaryName,
+		Short:   "Manage your duckcloud instance in your terminal.",
+		Version: buildinfos.Version,
 	}
 
 	// Generic flags
