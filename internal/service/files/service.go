33530020f2f8edeeb404473b75d18d0385b15a6f
Peltoche <pierre.peltier@protonmail.com>
2023-10-13 14:49:10 +0200

    Normalize the files errors

--- internal/service/files/service.go
+++ internal/service/files/service.go
@@ -10,13 +10,14 @@ import (
 	"github.com/spf13/afero"
 	"github.com/theduckcompany/duckcloud/internal/service/inodes"
 	"github.com/theduckcompany/duckcloud/internal/tools"
+	"github.com/theduckcompany/duckcloud/internal/tools/errs"
 	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
 )
 
 var (
-	ErrInvalidPath  = errors.New("invalid path")
-	ErrDirNotExists = errors.New("dir doesn't exists")
-	ErrNotAFile     = errors.New("not a file")
+	ErrInvalidPath   = errors.New("invalid path")
+	ErrInodeNotAFile = errors.New("inode doesn't point to a file")
+	ErrNotExist      = errors.New("file not exists")
 )
 
 type FSService struct {
@@ -58,7 +59,7 @@ func (s *FSService) Create(ctx context.Context) (afero.File, uuid.UUID, error) {
 
 	file, err := s.Open(ctx, fileID)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to open the file: %w", err)
+		return nil, "", errs.Internal(fmt.Errorf("failed to open the file: %w", err))
 	}
 
 	return file, fileID, nil
@@ -69,8 +70,11 @@ func (s *FSService) Open(ctx context.Context, fileID uuid.UUID) (afero.File, err
 	filePath := path.Join(idStr[:2], idStr)
 
 	file, err := s.fs.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0o600)
+	if os.IsNotExist(err) {
+		return nil, errs.BadRequest(ErrNotExist)
+	}
 	if err != nil {
-		return nil, fmt.Errorf("failed to Open %q: %w", filePath, err)
+		return nil, errs.Internal(fmt.Errorf("failed to Open %q: %w", filePath, err))
 	}
 
 	return file, nil
@@ -79,11 +83,16 @@ func (s *FSService) Open(ctx context.Context, fileID uuid.UUID) (afero.File, err
 func (s *FSService) Delete(ctx context.Context, inode *inodes.INode) error {
 	fileID := inode.FileID()
 	if fileID == nil {
-		return ErrNotAFile
+		return errs.BadRequest(ErrInodeNotAFile)
 	}
 
 	idStr := string(*fileID)
 	filePath := path.Join(idStr[:2], idStr)
 
-	return s.fs.Remove(filePath)
+	err := s.fs.Remove(filePath)
+	if err != nil {
+		return errs.Internal(err)
+	}
+
+	return nil
 }
