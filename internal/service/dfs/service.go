8a826a1c5f505d3ee14eb0f09de4a120206be9c9
Pierre Peltier <pierre.peltier@protonmail.com>
2023-10-16 16:52:36 +0200

    Normalize all the dfs paths with a beginning "/" (#357)

--- internal/service/dfs/service.go
+++ internal/service/dfs/service.go
@@ -2,6 +2,7 @@ package dfs
 
 import (
 	"errors"
+	"path"
 
 	"github.com/theduckcompany/duckcloud/internal/service/dfs/uploads"
 	"github.com/theduckcompany/duckcloud/internal/service/files"
@@ -27,4 +28,13 @@ func NewFSService(inodes inodes.Service, files files.Service, folders folders.Se
 
 func (s *FSService) GetFolderFS(folder *folders.Folder) FS {
 	return newLocalFS(s.inodes, s.files, folder, s.folders, s.uploads)
+}
+
+// cleanPath is equivalent to but slightly more efficient than
+// path.Clean("/" + name).
+func cleanPath(name string) string {
+	if name == "" || name[0] != '/' {
+		name = "/" + name
+	}
+	return path.Clean(name)
 }
