10e741d42c435f22f359bd296e6436a9f2d4b6ab
Peltoche <pierre.peltier@protonmail.com>
2023-10-18 09:49:49 +0200

    Add the runner to the Invoke list for the "Run" command

--- internal/server/run.go
+++ internal/server/run.go
@@ -5,13 +5,14 @@ import (
 	"database/sql"
 
 	"github.com/spf13/afero"
+	"github.com/theduckcompany/duckcloud/internal/service/tasks/runner"
 	"github.com/theduckcompany/duckcloud/internal/tools/router"
 	"go.uber.org/fx"
 )
 
 func Run(ctx context.Context, db *sql.DB, fs afero.Fs, folderPath string) {
 	// Start server with the HTTP server.
-	app := start(ctx, db, fs, folderPath, fx.Invoke(func(*router.API) {}))
+	app := start(ctx, db, fs, folderPath, fx.Invoke(func(*router.API, runner.Service) {}))
 
 	app.Run()
 }
