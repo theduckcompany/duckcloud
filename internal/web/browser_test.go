8b70ee32b3262187843bc75a69d3c1e49241093a
Peltoche <pierre.peltier@protonmail.com>
2023-11-02 10:10:20 +0100

    Add the mkdir command inside the browser

--- internal/web/browser_test.go
+++ internal/web/browser_test.go
@@ -2,11 +2,14 @@ package web
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
@@ -19,6 +22,7 @@ import (
 	"github.com/theduckcompany/duckcloud/internal/service/users"
 	"github.com/theduckcompany/duckcloud/internal/service/websessions"
 	"github.com/theduckcompany/duckcloud/internal/tools"
+	"github.com/theduckcompany/duckcloud/internal/tools/errs"
 	"github.com/theduckcompany/duckcloud/internal/tools/storage"
 	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
 	"github.com/theduckcompany/duckcloud/internal/web/html"
@@ -479,5 +483,376 @@ func Test_Browser_Page(t *testing.T) {
 		res := w.Result()
 		defer res.Body.Close()
 		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("getCreateDirModel success", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		htmlMock.On("WriteHTML", mock.Anything, mock.Anything, http.StatusOK, "browser/create-dir.tmpl", map[string]any{
+			"directory": "/foo/bar",
+			"folderID":  "some-folder-id",
+		}).Once()
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/browser/create-dir", nil)
+		queries := url.Values{}
+		queries.Add("dir", "/foo/bar")
+		queries.Add("folder", "some-folder-id")
+
+		r.URL.RawQuery = queries.Encode()
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("getCreateDirModel with no auth", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(nil, websessions.ErrSessionNotFound).Once()
+
+		webSessionsMock.On("Logout", mock.Anything, mock.Anything).Return(nil).Once()
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/browser/create-dir", nil)
+		queries := url.Values{}
+		queries.Add("dir", "/foo/bar")
+		queries.Add("folder", "some-folder-id")
+
+		r.URL.RawQuery = queries.Encode()
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("getCreateDirModel with no dir query", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		htmlMock.On("WriteHTMLErrorPage", mock.Anything, mock.Anything, errors.New("failed to get the dir path from the url query")).Once()
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/browser/create-dir", nil)
+		queries := url.Values{}
+		// queries.Add("dir", "/foo/bar")
+		queries.Add("folder", "some-folder-id")
+
+		r.URL.RawQuery = queries.Encode()
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("getCreateDirModel with no dir query", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		htmlMock.On("WriteHTMLErrorPage", mock.Anything, mock.Anything, errors.New("failed to get the folder id from the url query")).Once()
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/browser/create-dir", nil)
+		queries := url.Values{}
+		queries.Add("dir", "/foo/bar")
+		// queries.Add("folder", "some-folder-id")
+
+		r.URL.RawQuery = queries.Encode()
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("handleCreateDirReq", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		tools.UUIDMock.On("Parse", "some-folder-id").Return(uuid.UUID("some-folder-id"), nil).Once()
+		foldersMock.On("GetUserFolder", mock.Anything, users.ExampleAlice.ID(), uuid.UUID("some-folder-id")).
+			Return(&folders.ExampleAlicePersonalFolder, nil).Once()
+
+		folderFSMock := dfs.NewMockFS(t)
+		fsMock.On("GetFolderFS", &folders.ExampleAlicePersonalFolder).Return(folderFSMock)
+
+		folderFSMock.On("Get", mock.Anything, "/foo/New Dir").Return(nil, errs.ErrNotFound).Once()
+		folderFSMock.On("CreateDir", mock.Anything, "/foo/New Dir").Return(&dfs.ExampleAliceDir, nil).Once()
+
+		// Render
+		folderFSMock.On("Get", mock.Anything, "/foo").Return(&dfs.ExampleAliceRoot, nil).Once()
+		folderFSMock.On("Folder").Return(&folders.ExampleAlicePersonalFolder).Once()
+		folderFSMock.On("ListDir", mock.Anything, "/foo", &storage.PaginateCmd{
+			StartAfter: map[string]string{"name": ""},
+			Limit:      PageSize,
+		}).Return([]dfs.INode{dfs.ExampleAliceFile, dfs.ExampleAliceFile2}, nil).Once()
+		foldersMock.On("GetAllUserFolders", mock.Anything, users.ExampleAlice.ID(), (*storage.PaginateCmd)(nil)).
+			Return([]folders.Folder{folders.ExampleAlicePersonalFolder, folders.ExampleAliceBobSharedFolder}, nil).Once()
+		htmlMock.On("WriteHTML", mock.Anything, mock.Anything, http.StatusOK, "browser/content.tmpl", map[string]any{
+			"breadcrumb": []breadCrumbElement{
+				{
+					Name:    "Alice's Folder",
+					Href:    "/browser/e97b60f7-add2-43e1-a9bd-e2dac9ce69ec",
+					Current: false,
+				},
+				{
+					Name:    "",
+					Href:    "/browser/e97b60f7-add2-43e1-a9bd-e2dac9ce69ec",
+					Current: false,
+				},
+				{
+					Name:    "foo",
+					Href:    "/browser/e97b60f7-add2-43e1-a9bd-e2dac9ce69ec/foo",
+					Current: true,
+				},
+			},
+			"folder":   &folders.ExampleAlicePersonalFolder,
+			"folders":  []folders.Folder{folders.ExampleAlicePersonalFolder, folders.ExampleAliceBobSharedFolder},
+			"fullPath": "/foo",
+			"host":     "example.com",
+			"inodes":   []dfs.INode{dfs.ExampleAliceFile, dfs.ExampleAliceFile2},
+		})
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("handleCreateDirReq with an authentication error", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(nil, websessions.ErrMissingSessionToken).Once()
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+		assert.Equal(t, http.StatusFound, res.StatusCode)
+		assert.Equal(t, "/login", res.Header.Get("Location"))
+	})
+
+	t.Run("handleCreateDirReq with an invalid folderID", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		tools.UUIDMock.On("Parse", "some-folder-id").Return(uuid.UUID(""), fmt.Errorf("some-error")).Once()
+		htmlMock.On("WriteHTMLErrorPage", mock.Anything, mock.Anything, errors.New("invalid folder id param")).Once()
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		// form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("handleCreateDirReq with an empty name", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		tools.UUIDMock.On("Parse", "some-folder-id").Return(uuid.UUID("some-folder-id"), nil).Once()
+		htmlMock.On("WriteHTML", mock.Anything, mock.Anything, http.StatusUnprocessableEntity, "browser/create-dir.tmpl", map[string]any{
+			"directory": "/foo",
+			"folderID":  uuid.UUID("some-folder-id"),
+			"error":     "Must not be empty",
+		}).Once()
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		// form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("handleCreateDirReq with a taken name", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		tools.UUIDMock.On("Parse", "some-folder-id").Return(uuid.UUID("some-folder-id"), nil).Once()
+		foldersMock.On("GetUserFolder", mock.Anything, users.ExampleAlice.ID(), uuid.UUID("some-folder-id")).
+			Return(&folders.ExampleAlicePersonalFolder, nil).Once()
+
+		folderFSMock := dfs.NewMockFS(t)
+		fsMock.On("GetFolderFS", &folders.ExampleAlicePersonalFolder).Return(folderFSMock)
+
+		folderFSMock.On("Get", mock.Anything, "/foo/New Dir").Return(&dfs.ExampleAliceDir, nil).Once()
+
+		// Render
+		htmlMock.On("WriteHTML", mock.Anything, mock.Anything, http.StatusUnprocessableEntity, "browser/create-dir.tmpl", map[string]any{
+			"directory": "/foo",
+			"folderID":  uuid.UUID("some-folder-id"),
+			"error":     "Already exists",
+		}).Once()
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
+	})
+
+	t.Run("handleCreateDirReq with a CreateDir error", func(t *testing.T) {
+		tools := tools.NewMock(t)
+		webSessionsMock := websessions.NewMockService(t)
+		foldersMock := folders.NewMockService(t)
+		usersMock := users.NewMockService(t)
+		htmlMock := html.NewMockWriter(t)
+		auth := NewAuthenticator(webSessionsMock, usersMock, htmlMock)
+		fsMock := dfs.NewMockService(t)
+		handler := newBrowserHandler(tools, htmlMock, foldersMock, auth, fsMock)
+
+		// Authentication
+		webSessionsMock.On("GetFromReq", mock.Anything, mock.Anything).Return(&websessions.AliceWebSessionExample, nil).Once()
+		usersMock.On("GetByID", mock.Anything, users.ExampleAlice.ID()).Return(&users.ExampleAlice, nil).Once()
+
+		tools.UUIDMock.On("Parse", "some-folder-id").Return(uuid.UUID("some-folder-id"), nil).Once()
+		foldersMock.On("GetUserFolder", mock.Anything, users.ExampleAlice.ID(), uuid.UUID("some-folder-id")).
+			Return(&folders.ExampleAlicePersonalFolder, nil).Once()
+
+		folderFSMock := dfs.NewMockFS(t)
+		fsMock.On("GetFolderFS", &folders.ExampleAlicePersonalFolder).Return(folderFSMock)
+
+		err := fmt.Errorf("some-error")
+		folderFSMock.On("Get", mock.Anything, "/foo/New Dir").Return(nil, errs.ErrNotFound).Once()
+		folderFSMock.On("CreateDir", mock.Anything, "/foo/New Dir").Return(nil, err).Once()
+
+		htmlMock.On("WriteHTMLErrorPage", mock.Anything, mock.Anything, fmt.Errorf("failed to create the directory: %w", err)).Once()
+
+		w := httptest.NewRecorder()
+		form := url.Values{}
+		form.Add("dirPath", "/foo")
+		form.Add("name", "New Dir")
+		form.Add("folderID", "some-folder-id")
+		r := httptest.NewRequest(http.MethodPost, "/browser/create-dir", strings.NewReader(form.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		srv := chi.NewRouter()
+		handler.Register(srv, nil)
+		srv.ServeHTTP(w, r)
 	})
 }
