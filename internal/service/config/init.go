12b1ee4834028d4137eaf1784775006d27bec512
Peltoche <pierre.peltier@protonmail.com>
2023-11-17 11:31:53 +0100

    Refactor the config package to handle the master key

--- internal/service/config/init.go
+++ internal/service/config/init.go
@@ -3,24 +3,14 @@ package config
 import (
 	"context"
 	"database/sql"
+
+	"github.com/theduckcompany/duckcloud/internal/tools/secret"
 )
 
 //go:generate mockery --name Service
 type Service interface {
-	SetHostName(ctx context.Context, hostName string) error
-	SetAddrs(ctx context.Context, hosts []string, port int) error
-	SetTrustedHosts(ctx context.Context, hosts []string) error
-	EnableTLS(ctx context.Context) error
-	EnableDevMode(ctx context.Context) error
-	DisableDevMode(ctx context.Context) error
-	SetSSLPaths(ctx context.Context, certifPath, privateKeyPath string) error
-	DisableTLS(ctx context.Context) error
-	IsTLSEnabled(ctx context.Context) (bool, error)
-	IsDevModeEnabled(ctx context.Context) (bool, error)
-	GetTrustedHosts(ctx context.Context) ([]string, error)
-	GetSSLPaths(ctx context.Context) (string, string, error)
-	GetAddrs(ctx context.Context) ([]string, error)
-	GetHostName(ctx context.Context) (string, error)
+	SetMasterKey(ctx context.Context, key *secret.Key) error
+	GetMasterKey(ctx context.Context) (*secret.Key, error)
 }
 
 func Init(db *sql.DB) Service {
