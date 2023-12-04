5f7085a29487034984c631c217dbc3d05bae61ac
Peltoche <pierre.peltier@protonmail.com>
2023-12-04 11:51:08 +0100

    Copy the inodes storage into the dfs package

---
+++ internal/service/dfs/model_examples.go
@@ -0,0 +1,88 @@
+package dfs
+
+import (
+	"time"
+
+	inodes "github.com/theduckcompany/duckcloud/internal/service/dfs/internal/inodes"
+	"github.com/theduckcompany/duckcloud/internal/service/files"
+	"github.com/theduckcompany/duckcloud/internal/service/spaces"
+	"github.com/theduckcompany/duckcloud/internal/service/users"
+	"github.com/theduckcompany/duckcloud/internal/tools/ptr"
+	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
+)
+
+var (
+	now  = time.Now().UTC()
+	now2 = time.Now().Add(time.Minute).UTC()
+)
+
+var ExampleAliceRoot INode = inodes.INode{
+	id:             uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f"),
+	name:           "",
+	parent:         nil,
+	size:           0,
+	createdAt:      now,
+	createdBy:      users.ExampleAlice.ID(),
+	lastModifiedAt: now2,
+	fileID:         nil,
+}
+
+var ExampleAliceDir INode = INode{
+	id:             uuid.UUID("5592dac5-55f4-4206-87ca-8f31fc05e506"),
+	name:           "dir-a",
+	parent:         ptr.To(uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f")),
+	spaceID:        spaces.ExampleAlicePersonalSpace.ID(),
+	size:           42,
+	createdAt:      now,
+	createdBy:      users.ExampleAlice.ID(),
+	lastModifiedAt: now2,
+	fileID:         nil,
+}
+
+var ExampleAliceFile2 INode = INode{
+	id:             uuid.UUID("733da385-5cb8-4851-af12-927c252b6c1f "),
+	name:           "file.txt",
+	parent:         ptr.To(uuid.UUID("5592dac5-55f4-4206-87ca-8f31fc05e506")),
+	spaceID:        spaces.ExampleAlicePersonalSpace.ID(),
+	size:           42,
+	createdAt:      now,
+	createdBy:      users.ExampleAlice.ID(),
+	lastModifiedAt: now2,
+	fileID:         ptr.To(uuid.UUID("2007688f-022f-4d4a-a704-e86d66070227")),
+}
+
+var ExampleAliceFile INode = INode{
+	id:             uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f"),
+	name:           "foo.pdf",
+	parent:         ptr.To(uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f")),
+	spaceID:        spaces.ExampleAlicePersonalSpace.ID(),
+	size:           42,
+	createdAt:      now,
+	createdBy:      users.ExampleAlice.ID(),
+	lastModifiedAt: now2,
+	fileID:         ptr.To(files.ExampleFile1.ID()),
+}
+
+var ExampleAliceRenamedINode = INode{
+	id:             uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f"),
+	name:           "bar.pdf",
+	parent:         ptr.To(uuid.UUID("f5c0d3d2-e1b9-492b-b5d4-bd64bde0128f")),
+	spaceID:        spaces.ExampleAlicePersonalSpace.ID(),
+	size:           42,
+	createdAt:      now,
+	createdBy:      users.ExampleAlice.ID(),
+	lastModifiedAt: now2,
+	fileID:         ptr.To(files.ExampleFile1.ID()),
+}
+
+var ExampleBobRoot INode = INode{
+	id:             uuid.UUID("0923c86c-24b6-4b9d-9050-e82b8408edf4"),
+	name:           "",
+	parent:         nil,
+	spaceID:        spaces.ExampleBobPersonalSpace.ID(),
+	size:           0,
+	createdAt:      now,
+	createdBy:      users.ExampleBob.ID(),
+	lastModifiedAt: now2,
+	fileID:         nil,
+}
---
+++ internal/service/dfs/storage_sql.go
@@ -0,0 +1,216 @@
+package dfs
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/theduckcompany/duckcloud/internal/tools"
+	"github.com/theduckcompany/duckcloud/internal/tools/clock"
+	"github.com/theduckcompany/duckcloud/internal/tools/storage"
+	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
+)
+
+const tableName = "fs_inodes"
+
+var errNotFound = errors.New("not found")
+
+var allFiels = []string{"id", "name", "parent", "space_id", "size", "last_modified_at", "created_at", "created_by", "file_id"}
+
+type sqlStorage struct {
+	db    *sql.DB
+	clock clock.Clock
+}
+
+func newSqlStorage(db *sql.DB, tools tools.Tools) *sqlStorage {
+	return &sqlStorage{db, tools.Clock()}
+}
+
+func (s *sqlStorage) Save(ctx context.Context, i *INode) error {
+	_, err := sq.
+		Insert(tableName).
+		Columns(allFiels...).
+		Values(i.id, i.name, i.parent, i.spaceID, i.size, i.lastModifiedAt, i.createdAt, i.createdBy, i.fileID).
+		RunWith(s.db).
+		ExecContext(ctx)
+	if err != nil {
+		return fmt.Errorf("sql error: %w", err)
+	}
+
+	return nil
+}
+
+func (s *sqlStorage) GetSpaceRoot(ctx context.Context, spaceID uuid.UUID) (*INode, error) {
+	return s.getByKeys(ctx, sq.Eq{"space_id": spaceID, "parent": nil})
+}
+
+func (s *sqlStorage) Patch(ctx context.Context, inode uuid.UUID, fields map[string]any) error {
+	_, err := sq.Update(tableName).
+		SetMap(fields).
+		Where(sq.Eq{"id": inode}).
+		RunWith(s.db).
+		ExecContext(ctx)
+	if err != nil {
+		return fmt.Errorf("sql error: %w", err)
+	}
+
+	return nil
+}
+
+func (s *sqlStorage) GetByID(ctx context.Context, id uuid.UUID) (*INode, error) {
+	return s.getByKeys(ctx, sq.Eq{"id": id})
+}
+
+func (s *sqlStorage) HardDelete(ctx context.Context, id uuid.UUID) error {
+	_, err := sq.
+		Delete(tableName).
+		Where(sq.Eq{"id": string(id)}).
+		RunWith(s.db).
+		ExecContext(ctx)
+	if err != nil {
+		return fmt.Errorf("sql error: %w", err)
+	}
+
+	return nil
+}
+
+func (s *sqlStorage) GetAllChildrens(ctx context.Context, parent uuid.UUID, cmd *storage.PaginateCmd) ([]INode, error) {
+	rows, err := storage.PaginateSelection(sq.
+		Select(allFiels...).
+		Where(sq.Eq{"parent": string(parent), "deleted_at": nil}).
+		From(tableName), cmd).
+		RunWith(s.db).
+		QueryContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("sql error: %w", err)
+	}
+
+	defer rows.Close()
+
+	return s.scanRows(rows)
+}
+
+func (s *sqlStorage) GetDeleted(ctx context.Context, id uuid.UUID) (*INode, error) {
+	return s.getByKeys(ctx, sq.Eq{"id": id}, sq.NotEq{"deleted_at": nil})
+}
+
+func (s *sqlStorage) GetAllDeleted(ctx context.Context, limit int) ([]INode, error) {
+	rows, err := sq.
+		Select(allFiels...).
+		From(tableName).
+		Where(sq.NotEq{"deleted_at": nil}).
+		Limit(uint64(limit)).
+		RunWith(s.db).
+		QueryContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("sql error: %w", err)
+	}
+
+	defer rows.Close()
+
+	return s.scanRows(rows)
+}
+
+func (s *sqlStorage) scanRows(rows *sql.Rows) ([]INode, error) {
+	inodes := []INode{}
+
+	for rows.Next() {
+		var res INode
+
+		err := rows.Scan(&res.id, &res.name, &res.parent, &res.spaceID, &res.size, &res.lastModifiedAt, &res.createdAt, &res.createdBy, &res.fileID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan a row: %w", err)
+		}
+
+		inodes = append(inodes, res)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("scan error: %w", err)
+	}
+
+	return inodes, nil
+}
+
+func (s *sqlStorage) GetSumChildsSize(ctx context.Context, parent uuid.UUID) (uint64, error) {
+	var size *uint64
+
+	err := sq.
+		Select("SUM(size)").
+		From(tableName).
+		Where(sq.Eq{"parent": string(parent), "deleted_at": nil}).
+		RunWith(s.db).
+		ScanContext(ctx, &size)
+	if err != nil {
+		return 0, fmt.Errorf("sql error: %w", err)
+	}
+
+	if size == nil {
+		return 0, nil
+	}
+
+	return *size, nil
+}
+
+func (s *sqlStorage) GetAllInodesWithFileID(ctx context.Context, fileID uuid.UUID) ([]INode, error) {
+	rows, err := sq.
+		Select(allFiels...).
+		From(tableName).
+		Where(sq.Eq{"file_id": fileID}).
+		RunWith(s.db).
+		QueryContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("sql error: %w", err)
+	}
+
+	defer rows.Close()
+
+	return s.scanRows(rows)
+}
+
+func (s *sqlStorage) GetByNameAndParent(ctx context.Context, name string, parent uuid.UUID) (*INode, error) {
+	res := INode{}
+
+	err := sq.
+		Select(allFiels...).
+		From(tableName).
+		Where(sq.Eq{"parent": string(parent), "name": name, "deleted_at": nil}).
+		RunWith(s.db).
+		ScanContext(ctx, &res.id, &res.name, &res.parent, &res.spaceID, &res.size, &res.lastModifiedAt, &res.createdAt, &res.createdBy, &res.fileID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, errNotFound
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("sql error: %w", err)
+	}
+
+	return &res, nil
+}
+
+func (s *sqlStorage) getByKeys(ctx context.Context, wheres ...any) (*INode, error) {
+	res := INode{}
+
+	query := sq.
+		Select(allFiels...).
+		From(tableName)
+
+	for _, where := range wheres {
+		query = query.Where(where)
+	}
+
+	err := query.
+		RunWith(s.db).
+		ScanContext(ctx, &res.id, &res.name, &res.parent, &res.spaceID, &res.size, &res.lastModifiedAt, &res.createdAt, &res.createdBy, &res.fileID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, errNotFound
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("sql error: %w", err)
+	}
+
+	return &res, nil
+}
