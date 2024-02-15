875cc2d6dca52594890e8274f52473e93c45a81c
Pierre Peltier <pierre.peltier@protonmail.com>
2024-02-15 21:37:06 +0100

    Fix the sidenav loading (#543)
    When a new page was loaded via an HTMX request the interactions with the
    sidenav was broken. This was due to the content of "sidenav.js" not
    running. The fact that the code was not running was due to the use of
    module which are loaded only once.
    
    By exporting the function and calling them explicitly inside the layout
    page we can be sure that the code will be always executed.

---
+++ assets/public/js/setup.mjs
@@ -0,0 +1,46 @@
+
+import {Sidenav, Dropdown, Select} from "/assets/js/libs/mdb.es.min.js";
+
+export function SetupSideNav() {
+  const sidenav = document.getElementById("main-sidenav");
+
+  let innerWidth = null;
+
+  const setMode = (e) => {
+    const sidenavInstance = Sidenav.getOrCreateInstance(sidenav);
+    // Check necessary for Android devices
+    if (window.innerWidth === innerWidth) {
+      return;
+    }
+
+    innerWidth = window.innerWidth;
+
+    if (window.innerWidth < 1400) {
+      sidenavInstance.changeMode("over");
+      sidenavInstance.hide();
+    } else {
+      sidenavInstance.changeMode("side");
+      sidenavInstance.show();
+    }
+  };
+
+  setMode();
+
+  // Event listeners
+  window.addEventListener("resize", setMode);
+}
+
+
+export function SetupBoostrapElems() {
+  // Make all the selects pretty even with the dynamic content
+  document.body.addEventListener("htmx:afterSwap", function (evt) {
+    document.querySelectorAll('.select').forEach((select) => {
+      Select.getOrCreateInstance(select);
+    });
+
+    document.querySelectorAll('.dropdown').forEach((dropdown) => {
+      Dropdown.getOrCreateInstance(dropdown);
+    });
+  })
+}
+
