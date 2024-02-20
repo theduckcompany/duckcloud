6f1341919980c6616149e8629c69a85a486de63a
Peltoche <pierre.peltier@protonmail.com>
2024-02-20 11:16:15 +0100

    Trigger the Upload button setup after each htmx rendering

--- assets/public/js/file-upload.mjs
+++ assets/public/js/file-upload.mjs
@@ -1,61 +1,64 @@
 import {Uppy, XHRUpload, StatusBar} from "/assets/js/libs/uppy.min.mjs"
 
-let client = new Uppy().use(XHRUpload, {
-  endpoint: '/browser/upload',
-  allowMultipleUploadBatches: true
-})
+export function setupUploadButton() {
 
-const folderPath = document.getElementById("folder-path-meta")
-const spaceID = document.getElementById("space-id-meta")
-
-client.setMeta({rootPath: folderPath.value})
-client.setMeta({spaceID: spaceID.value})
-client.use(StatusBar, {target: '#status-bar'});
-
-client.on('complete', (result) => {
-  htmx.trigger("body", "refreshFolder");
-});
-
-document.getElementById('upload-file-btn').
-  addEventListener("click", (e) => {
-    var input = document.createElement('input');
-    input.type = 'file';
-    input.multiple = true
-
-    input.onchange = e => {
-      for (const file of e.target.files) {
-        client.addFile(file)
-      }
-
-      client.upload()
-    }
-
-    input.click();
+  let client = new Uppy().use(XHRUpload, {
+    endpoint: '/browser/upload',
+    allowMultipleUploadBatches: true
   })
 
-document.getElementById('upload-folder-btn').
-  addEventListener("click", (e) => {
-    var input = document.createElement('input');
-    input.type = 'file';
-    input.multiple = true
-    input.webkitdirectory = true
-    input.mozdirectory = true
-    input.directory = true
+  const folderPath = document.getElementById("folder-path-meta")
+  const spaceID = document.getElementById("space-id-meta")
 
-    input.onchange = e => {
-      for (const file of e.target.files) {
-        client.addFile({
+  client.setMeta({rootPath: folderPath.value})
+  client.setMeta({spaceID: spaceID.value})
+  client.use(StatusBar, {target: '#status-bar'});
 
-          name: file.webkitRelativePath,
-          data: file,
-          type: file.type,
-          source: 'Local',
-          isRemote: false,
-        })
+  client.on('complete', (result) => {
+    htmx.trigger("body", "refreshFolder");
+  });
+
+  document.getElementById('upload-file-btn').
+    addEventListener("click", (e) => {
+      var input = document.createElement('input');
+      input.type = 'file';
+      input.multiple = true
+
+      input.onchange = e => {
+        for (const file of e.target.files) {
+          client.addFile(file)
+        }
+
+        client.upload()
       }
 
-      client.upload()
-    }
+      input.click();
+    })
 
-    input.click();
-  })
+  document.getElementById('upload-folder-btn').
+    addEventListener("click", (e) => {
+      var input = document.createElement('input');
+      input.type = 'file';
+      input.multiple = true
+      input.webkitdirectory = true
+      input.mozdirectory = true
+      input.directory = true
+
+      input.onchange = e => {
+        for (const file of e.target.files) {
+          client.addFile({
+
+            name: file.webkitRelativePath,
+            data: file,
+            type: file.type,
+            source: 'Local',
+            isRemote: false,
+          })
+        }
+
+        client.upload()
+      }
+
+      input.click();
+    })
+}
