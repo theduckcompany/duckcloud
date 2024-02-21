495596f3c658eb9bbe16309a236033a002f3f19e
Peltoche <pierre.peltier@protonmail.com>
2024-02-21 21:51:44 +0100

    Move the csp into a meta tag

--- internal/tools/router/http_router.go
+++ internal/tools/router/http_router.go
@@ -128,7 +128,6 @@ func createHandler(cfg Config, routes []Registerer, mids *Middlewares) (chi.Rout
 				w.Header().Set("X-Frame-Options", "DENY")
 				w.Header().Set("X-Content-Type-Options", "nosniff")
 				w.Header().Set("Referrer-Policy", "same-origin")
-				w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; upgrade-insecure-requests")
 				w.Header().Set("Permissions-Policy", "")
 
 				next.ServeHTTP(w, r)
