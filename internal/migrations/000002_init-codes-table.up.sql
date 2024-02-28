17a9a267e9c141c3ee40fac912699db044a723b1
Peltoche <pierre.peltier@protonmail.com>
2024-02-28 10:02:09 +0100

    Add a FOREIGN KEY for all the client_id references

--- internal/migrations/000002_init-codes-table.up.sql
+++ internal/migrations/000002_init-codes-table.up.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS oauth_codes (
   "challenge" TEXT DEFAULT NULL,
   "challenge_method" TEXT DEFAULT NULL,
   "scope" TEXT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(client_id) REFERENCES oauth_clients(id) 
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_oauth_codes_code ON oauth_codes(code);
--- internal/migrations/000004_init-oauth_sessions-table.up.sql
+++ internal/migrations/000004_init-oauth_sessions-table.up.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS oauth_sessions (
   "user_id" TEXT NOT NULL,
   "client_id" TEXT NOT NULL,
   "scope" TEXT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(client_id) REFERENCES oauth_clients(id) 
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_oauth_sessions_access_token ON oauth_sessions(access_token);
