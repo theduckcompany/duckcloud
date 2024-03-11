ae754c7d25b141e5f9ed2ca3e624536b14e16080
Pierre Peltier <pierre.peltier@protonmail.com>
2024-03-11 12:25:17 +0100

    Refactor the users tests (#593)

---
+++ internal/service/users/model_helper.go
@@ -0,0 +1,66 @@
+package users
+
+import (
+	"testing"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v7"
+
+	"github.com/theduckcompany/duckcloud/internal/tools/secret"
+	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
+)
+
+type FakeUserBuilder struct {
+	t    testing.TB
+	user *User
+}
+
+func NewFakeUser(t testing.TB) *FakeUserBuilder {
+	t.Helper()
+
+	uuidProvider := uuid.NewProvider()
+
+	createdAt := gofakeit.DateRange(time.Now().Add(-time.Hour*1000), time.Now())
+
+	return &FakeUserBuilder{
+		t: t,
+		user: &User{
+			id:                uuidProvider.New(),
+			createdAt:         createdAt,
+			passwordChangedAt: createdAt,
+			username:          gofakeit.Username(),
+			password:          secret.NewText(gofakeit.Password(true, true, true, false, false, 8)),
+			status:            Active,
+			createdBy:         uuidProvider.New(),
+			isAdmin:           false,
+		},
+	}
+}
+
+func (f *FakeUserBuilder) WithAdminRole() *FakeUserBuilder {
+	f.user.isAdmin = true
+
+	return f
+}
+
+func (f *FakeUserBuilder) WithStatus(status Status) *FakeUserBuilder {
+	f.user.status = status
+
+	return f
+}
+
+func (f *FakeUserBuilder) Build() *User {
+	return f.user
+}
+
+// func (f *FakeUserBuilder) BuildAndStore(db *sql.DB) *User {
+// 	f.t.Helper()
+//
+// 	tools := tools.NewToolboxForTest(f.t)
+// 	storage := newSqlStorage(db, tools)
+//
+// 	err := storage.Save(context.Background(), f.user)
+// 	require.NoError(f.t, err)
+//
+// 	return f.user
+// }
