4472352cfb60f872475b9d673cb303e0bd7162c8
Peltoche <pierre.peltier@protonmail.com>
2024-03-13 10:40:29 +0100

    Refactor the websessions tests

--- internal/service/websessions/service_test.go
+++ internal/service/websessions/service_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/theduckcompany/duckcloud/internal/service/users"
 	"github.com/theduckcompany/duckcloud/internal/tools"
 	"github.com/theduckcompany/duckcloud/internal/tools/errs"
 	"github.com/theduckcompany/duckcloud/internal/tools/secret"
@@ -20,182 +21,279 @@ import (
 )
 
 func Test_WebSessions_Service(t *testing.T) {
-	now := time.Now()
+	t.Parallel()
+
 	ctx := context.Background()
 
-	session := Session{
-		token:     secret.NewText("some-token"),
-		userID:    uuid.UUID("3a708fc5-dc10-4655-8fc2-33b08a4b33a5"),
-		ip:        "192.168.1.1",
-		device:    "Android - Chrome",
-		createdAt: now,
-	}
-
 	t.Run("Create success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		tools.UUIDMock.On("New").Return(uuid.UUID("some-token")).Once()
-		tools.ClockMock.On("Now").Return(now).Once()
-		storageMock.On("Save", mock.Anything, &session).Return(nil).Once()
+		// Data
+		now := time.Now().UTC()
+		rawToken := "some-token"
+		user := users.NewFakeUser(t).Build()
+		session := NewFakeSession(t).
+			WithToken(rawToken).
+			WithIP("192.168.1.1").
+			WithDevice("Android - Chrome").
+			CreatedAt(now).
+			CreatedBy(user).
+			Build()
 
+		// Mocks
+		tools.UUIDMock.On("New").Return(uuid.UUID(rawToken)).Once()
+		tools.ClockMock.On("Now").Return(now).Once()
+		storageMock.On("Save", mock.Anything, session).Return(nil).Once()
+
+		// Run
 		res, err := service.Create(ctx, &CreateCmd{
-			UserID:     "3a708fc5-dc10-4655-8fc2-33b08a4b33a5",
+			UserID:     user.ID(),
 			UserAgent:  "Mozilla/5.0 (Linux; Android 10; 8092) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
 			RemoteAddr: "192.168.1.1",
 		})
+
+		// Asserts
 		require.NoError(t, err)
-		assert.EqualValues(t, &session, res)
+		assert.EqualValues(t, session, res)
 	})
 
 	t.Run("Create with an invalid cmd", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+
+		// Mocks
+
+		// Run
 		res, err := service.Create(ctx, &CreateCmd{
 			UserID:     "not a uuid",
 			UserAgent:  "Mozilla/5.0 (Linux; Android 10; 8092) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
 			RemoteAddr: "192.168.1.1",
 		})
+
+		// Asserts
 		assert.Nil(t, res)
 		require.ErrorIs(t, err, errs.ErrValidation)
 		require.ErrorContains(t, err, "UserID: must be a valid UUID v4.")
 	})
 
 	t.Run("Create with a storage error", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		tools.UUIDMock.On("New").Return(uuid.UUID("some-token")).Once()
-		tools.ClockMock.On("Now").Return(now).Once()
-		storageMock.On("Save", mock.Anything, &session).Return(fmt.Errorf("some-error")).Once()
+		// Data
+		now := time.Now().UTC()
+		rawToken := "some-token"
+		user := users.NewFakeUser(t).Build()
+		session := NewFakeSession(t).
+			WithToken(rawToken).
+			WithIP("192.168.1.1").
+			WithDevice("Android - Chrome").
+			CreatedAt(now).
+			CreatedBy(user).
+			Build()
 
+		// Mocks
+		tools.UUIDMock.On("New").Return(uuid.UUID(rawToken)).Once()
+		tools.ClockMock.On("Now").Return(now).Once()
+		storageMock.On("Save", mock.Anything, session).Return(fmt.Errorf("some-error")).Once()
+
+		// Run
 		res, err := service.Create(ctx, &CreateCmd{
-			UserID:     "3a708fc5-dc10-4655-8fc2-33b08a4b33a5",
+			UserID:     user.ID(),
 			UserAgent:  "Mozilla/5.0 (Linux; Android 10; 8092) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
 			RemoteAddr: "192.168.1.1",
 		})
+
+		// Asserts
 		assert.Nil(t, res)
 		require.ErrorIs(t, err, errs.ErrInternal)
 		require.ErrorContains(t, err, "failed to save the session: some-error")
 	})
 
 	t.Run("GetAllForUser success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetAllForUser", mock.Anything, AliceWebSessionExample.userID, &sqlstorage.PaginateCmd{Limit: 10}).Return([]Session{AliceWebSessionExample}, nil).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		session := NewFakeSession(t).CreatedBy(user).Build()
 
-		res, err := service.GetAllForUser(ctx, AliceWebSessionExample.userID, &sqlstorage.PaginateCmd{Limit: 10})
+		// Mocks
+		storageMock.On("GetAllForUser", mock.Anything, user.ID(), &sqlstorage.PaginateCmd{Limit: 10}).Return([]Session{*session}, nil).Once()
+
+		// Run
+		res, err := service.GetAllForUser(ctx, user.ID(), &sqlstorage.PaginateCmd{Limit: 10})
+
+		// Asserts
 		require.NoError(t, err)
-		assert.Equal(t, []Session{AliceWebSessionExample}, res)
+		assert.Equal(t, []Session{*session}, res)
 	})
 
 	t.Run("GetByToken success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetByToken", mock.Anything, secret.NewText("some-token")).Return(&session, nil).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(user).Build()
 
-		res, err := service.GetByToken(ctx, secret.NewText("some-token"))
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, secret.NewText(rawToken)).Return(session, nil).Once()
+
+		// Run
+		res, err := service.GetByToken(ctx, secret.NewText(rawToken))
+
+		// Asserts
 		require.NoError(t, err)
-		assert.EqualValues(t, &session, res)
+		assert.EqualValues(t, session, res)
 	})
 
 	t.Run("GetFromReq success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
+
+		// Data
+		user := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(user).Build()
 
 		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 		req.AddCookie(&http.Cookie{
 			Name:  "session_token",
-			Value: "some-token",
+			Value: rawToken,
 		})
 
-		storageMock.On("GetByToken", mock.Anything, secret.NewText("some-token")).Return(&session, nil).Once()
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, secret.NewText(rawToken)).Return(session, nil).Once()
 
+		// Run
 		res, err := service.GetFromReq(req)
+
+		// Asserts
 		require.NoError(t, err)
-		assert.EqualValues(t, &session, res)
+		assert.EqualValues(t, session, res)
 	})
 
 	t.Run("GetFromReq with no cookie", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
-		// No cookie
+		// Data
+		req, _ := http.NewRequest(http.MethodGet, "/foo", nil) // No cookie
 
+		// Run
 		res, err := service.GetFromReq(req)
+
+		// Asserts
 		assert.Nil(t, res)
 		require.EqualError(t, err, "bad request: missing session token")
 	})
 
 	t.Run("GetFromReq with the session not found", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+		rawToken := "some-token"
 		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 		req.AddCookie(&http.Cookie{
 			Name:  "session_token",
-			Value: "some-token",
+			Value: rawToken,
 		})
 
-		storageMock.On("GetByToken", mock.Anything, secret.NewText("some-token")).Return(nil, errNotFound).Once()
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, secret.NewText(rawToken)).Return(nil, errNotFound).Once()
 
+		// Run
 		res, err := service.GetFromReq(req)
+
+		// Assets
 		assert.Nil(t, res)
 		require.ErrorIs(t, err, errs.ErrBadRequest)
 		require.ErrorIs(t, err, ErrSessionNotFound)
 	})
 
 	t.Run("GetFromReq with a storage error", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+		rawToken := "some-token"
 		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 		req.AddCookie(&http.Cookie{
 			Name:  "session_token",
-			Value: "some-token",
+			Value: rawToken,
 		})
 
-		storageMock.On("GetByToken", mock.Anything, secret.NewText("some-token")).Return(nil, errors.New("some-error")).Once()
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, secret.NewText(rawToken)).Return(nil, errors.New("some-error")).Once()
 
+		// Run
 		res, err := service.GetFromReq(req)
+
+		// Assets
 		assert.Nil(t, res)
 		require.ErrorIs(t, err, errs.ErrInternal)
 		require.ErrorContains(t, err, "some-error")
 	})
 
 	t.Run("Logout success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+		rawToken := "some-token"
 		w := httptest.NewRecorder()
-
 		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 		req.AddCookie(&http.Cookie{
 			Name:  "session_token",
-			Value: "some-token",
+			Value: rawToken,
 		})
 
-		storageMock.On("RemoveByToken", mock.Anything, secret.NewText("some-token")).Return(nil).Once()
+		// Mocks
+		storageMock.On("RemoveByToken", mock.Anything, secret.NewText(rawToken)).Return(nil).Once()
 
+		// Run
 		err := service.Logout(req, w)
+
+		// Asserts
 		require.NoError(t, err)
 
-		// Check that the session_token cookie is set to an empty value.
-		res := w.Result()
+		res := w.Result() // Check that the session_token cookie is set to an empty value.
 		res.Body.Close()
 		assert.Len(t, res.Cookies(), 1)
 		assert.Empty(t, res.Cookies()[0].Value)
@@ -203,134 +301,220 @@ func Test_WebSessions_Service(t *testing.T) {
 	})
 
 	t.Run("Logout with no cookie", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
 		w := httptest.NewRecorder()
 
-		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
-		// No cookie
+		// Data
+		req, _ := http.NewRequest(http.MethodGet, "/foo", nil) // No cookie
 
+		// Mocks
 		// Do nothing
 
+		// Run
 		err := service.Logout(req, w)
+
+		// Asserts
 		require.NoError(t, err)
 	})
 
 	t.Run("Logout with a storage error", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+		rawToken := "some-token"
 		w := httptest.NewRecorder()
-
 		req, _ := http.NewRequest(http.MethodGet, "/foo", nil)
 		req.AddCookie(&http.Cookie{
 			Name:  "session_token",
-			Value: "some-token",
+			Value: rawToken,
 		})
 
-		storageMock.On("RemoveByToken", mock.Anything, secret.NewText("some-token")).Return(errors.New("some-error")).Once()
+		// Mocks
+		storageMock.On("RemoveByToken", mock.Anything, secret.NewText(rawToken)).Return(errors.New("some-error")).Once()
 
+		// Run
 		err := service.Logout(req, w)
+
+		// Asserts
 		require.ErrorIs(t, err, errs.ErrInternal)
 		require.ErrorContains(t, err, "failed to remove the token: some-error")
 	})
 
 	t.Run("Delete success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetByToken", mock.Anything, AliceWebSessionExample.Token()).Return(&AliceWebSessionExample, nil).Once()
-		storageMock.On("RemoveByToken", mock.Anything, AliceWebSessionExample.Token()).Return(nil).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(user).Build()
 
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, session.Token()).Return(session, nil).Once()
+		storageMock.On("RemoveByToken", mock.Anything, session.Token()).Return(nil).Once()
+
+		// Run
 		err := service.Delete(ctx, &DeleteCmd{
-			UserID: AliceWebSessionExample.UserID(),
-			Token:  AliceWebSessionExample.Token(),
+			UserID: user.ID(),
+			Token:  session.Token(),
 		})
+
+		// Asserts
 		require.NoError(t, err)
 	})
 
 	t.Run("Delete with a validation error", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
+		// Data
+		session := NewFakeSession(t).Build()
+
+		// Run
 		err := service.Delete(ctx, &DeleteCmd{
 			UserID: "some-invalid-id",
-			Token:  AliceWebSessionExample.Token(),
+			Token:  session.Token(),
 		})
+
+		// Asserts
 		require.ErrorIs(t, err, errs.ErrValidation)
 		require.ErrorContains(t, err, "UserID: must be a valid UUID v4.")
 	})
 
 	t.Run("Delete with a token not found", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetByToken", mock.Anything, AliceWebSessionExample.Token()).Return(nil, errNotFound).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(user).Build()
 
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, session.Token()).Return(nil, errNotFound).Once()
+
+		// Run
 		err := service.Delete(ctx, &DeleteCmd{
-			UserID: AliceWebSessionExample.UserID(),
-			Token:  AliceWebSessionExample.Token(),
+			UserID: user.ID(),
+			Token:  session.Token(),
 		})
+
+		// Asserts
 		require.NoError(t, err)
 	})
 
 	t.Run("Delete with a token owned by someone else", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetByToken", mock.Anything, AliceWebSessionExample.Token()).Return(&AliceWebSessionExample, nil).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		anAnotherUser := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(anAnotherUser).Build()
 
+		// Mocks
+		storageMock.On("GetByToken", mock.Anything, session.Token()).Return(session, nil).Once()
+
+		// Run
 		err := service.Delete(ctx, &DeleteCmd{
-			UserID: uuid.UUID("29a81212-9e46-4678-a921-ecaf53aa15bc"), // A random user id
-			Token:  AliceWebSessionExample.Token(),
+			UserID: user.ID(),
+			Token:  session.Token(), // The sessions is owned by "anAnotherUser"
 		})
+
+		// Asserts
 		require.EqualError(t, err, "not found: user ids are not matching")
 		require.ErrorIs(t, err, errs.ErrNotFound)
 	})
 
 	t.Run("DeleteAll success", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetAllForUser", mock.Anything, AliceWebSessionExample.UserID(), (*sqlstorage.PaginateCmd)(nil)).Return([]Session{AliceWebSessionExample}, nil).Once()
-		storageMock.On("GetByToken", mock.Anything, AliceWebSessionExample.Token()).Return(&AliceWebSessionExample, nil).Once()
-		storageMock.On("RemoveByToken", mock.Anything, AliceWebSessionExample.Token()).Return(nil).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
+		rawToken := "some-token"
+		session := NewFakeSession(t).WithToken(rawToken).CreatedBy(user).Build()
 
-		err := service.DeleteAll(ctx, AliceWebSessionExample.UserID())
+		// Mocks
+		storageMock.On("GetAllForUser", mock.Anything, user.ID(), (*sqlstorage.PaginateCmd)(nil)).Return([]Session{*session}, nil).Once()
+		storageMock.On("GetByToken", mock.Anything, session.Token()).Return(session, nil).Once()
+		storageMock.On("RemoveByToken", mock.Anything, session.Token()).Return(nil).Once()
+
+		// Run
+		err := service.DeleteAll(ctx, user.ID())
+
+		// Asserts
 		require.NoError(t, err)
 	})
 
 	t.Run("DeleteAll with a GetAll error", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetAllForUser", mock.Anything, AliceWebSessionExample.UserID(), (*sqlstorage.PaginateCmd)(nil)).Return(nil, fmt.Errorf("some-error")).Once()
+		// Data
+		user := users.NewFakeUser(t).Build()
 
-		err := service.DeleteAll(ctx, AliceWebSessionExample.UserID())
+		// Mocks
+		storageMock.On("GetAllForUser", mock.Anything, user.ID(), (*sqlstorage.PaginateCmd)(nil)).Return(nil, fmt.Errorf("some-error")).Once()
+
+		// Run
+		err := service.DeleteAll(ctx, user.ID())
+
+		// Asserts
 		require.ErrorIs(t, err, errs.ErrInternal)
 		require.ErrorContains(t, err, "failed to GetAllForUser: some-error")
 	})
 
 	t.Run("DeleteAll with a revoke error stop directly", func(t *testing.T) {
+		t.Parallel()
+
 		tools := tools.NewMock(t)
 		storageMock := newMockStorage(t)
 		service := newService(storageMock, tools)
 
-		storageMock.On("GetAllForUser", mock.Anything, AliceWebSessionExample.UserID(), (*sqlstorage.PaginateCmd)(nil)).Return([]Session{AliceWebSessionExample, AliceWebSessionExample}, nil).Once()
-		storageMock.On("GetByToken", mock.Anything, AliceWebSessionExample.Token()).Return(&AliceWebSessionExample, nil).Once()
-		storageMock.On("RemoveByToken", mock.Anything, AliceWebSessionExample.Token()).Return(fmt.Errorf("some-error")).Once()
-		// Do not call GetByToken and RemoveByToken a second time
+		// Data
+		user := users.NewFakeUser(t).Build()
+		session := NewFakeSession(t).CreatedBy(user).Build()
+		session2 := NewFakeSession(t).CreatedBy(user).Build()
 
-		err := service.DeleteAll(ctx, AliceWebSessionExample.UserID())
+		// Mocks
+		storageMock.On("GetAllForUser", mock.Anything, user.ID(), (*sqlstorage.PaginateCmd)(nil)).Return([]Session{*session, *session2}, nil).Once()
+		storageMock.On("GetByToken", mock.Anything, session.Token()).Return(session, nil).Once()
+		storageMock.On("RemoveByToken", mock.Anything, session.Token()).Return(fmt.Errorf("some-error")).Once()
+		// Do not call GetByToken and RemoveByToken for "session2"
+
+		// Run
+		err := service.DeleteAll(ctx, user.ID())
+
+		// Asserts
 		require.ErrorIs(t, err, errs.ErrInternal)
 		require.ErrorContains(t, err, "some-error")
 	})
