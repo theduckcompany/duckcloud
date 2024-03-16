af5203571bf53380b93b5fb88c0a61135f12a802
Peltoche <pierre.peltier@protonmail.com>
2024-03-16 10:54:16 +0100

    Refactor the oauthcodes sql tests

--- internal/service/oauthcodes/storage_sql_test.go
+++ internal/service/oauthcodes/storage_sql_test.go
@@ -3,65 +3,68 @@ package oauthcodes
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/theduckcompany/duckcloud/internal/service/oauthclients"
+	"github.com/theduckcompany/duckcloud/internal/service/users"
 	"github.com/theduckcompany/duckcloud/internal/tools/secret"
 	"github.com/theduckcompany/duckcloud/internal/tools/sqlstorage"
 )
 
 func TestOauthCodeSQLStorage(t *testing.T) {
 	ctx := context.Background()
-	now := time.Now().UTC()
-
-	codeExample := Code{
-		code:            secret.NewText("some-code"),
-		createdAt:       now,
-		expiresAt:       now.Add(time.Hour),
-		clientID:        "some-client-id",
-		userID:          "some-user-id",
-		redirectURI:     "http://some-redirect.com/uri",
-		scope:           "some-scope",
-		challenge:       secret.NewText("some-challenge"),
-		challengeMethod: "plain",
-	}
 
 	db := sqlstorage.NewTestStorage(t)
 	storage := newSqlStorage(db)
 
-	t.Run("save", func(t *testing.T) {
-		err := storage.Save(context.Background(), &codeExample)
+	user := users.NewFakeUser(t).BuildAndStore(ctx, db)
+	oauthclients := oauthclients.NewFakeClient(t).CreatedBy(user).BuildAndStore(ctx, db)
+	code := NewFakeCode(t).WithClient(oauthclients).CreatedBy(user).Build()
 
+	t.Run("save", func(t *testing.T) {
+		// Run
+		err := storage.Save(ctx, code)
+
+		// Asserts
 		require.NoError(t, err)
 	})
 
 	t.Run("GetByID ok", func(t *testing.T) {
-		code, err := storage.GetByCode(context.Background(), secret.NewText("some-code"))
+		// Run
+		res, err := storage.GetByCode(context.Background(), code.code)
 
+		// Asserts
 		require.NoError(t, err)
-		assert.EqualValues(t, &codeExample, code)
+		assert.EqualValues(t, code, res)
 	})
 
 	t.Run("GetByID not found", func(t *testing.T) {
+		// Run
 		code, err := storage.GetByCode(ctx, secret.NewText("some-invalid-code"))
 
+		// Asserts
 		assert.Nil(t, code)
 		require.ErrorIs(t, err, errNotFound)
 	})
 
 	t.Run("RemoveByCode success", func(t *testing.T) {
-		err := storage.RemoveByCode(ctx, secret.NewText("some-code"))
-		require.NoError(t, err)
+		// Run
+		err := storage.RemoveByCode(ctx, code.code)
 
+		// Asserts
+		require.NoError(t, err)
 		// Check that the code is no more available
-		code, err := storage.GetByCode(ctx, secret.NewText("some-code"))
+		code, err := storage.GetByCode(ctx, code.code)
 		assert.Nil(t, code)
 		require.ErrorIs(t, err, errNotFound)
 	})
 
 	t.Run("RemoveByCode invalid code", func(t *testing.T) {
+		// Run
 		err := storage.RemoveByCode(ctx, secret.NewText("some-invalid-code"))
+
+		// Asserts
 		require.NoError(t, err)
 	})
 }
