02c973b41f0c8ff062ecdbbf66f675ba39d6eb27
Pierre Peltier <pierre.peltier@protonmail.com>
2024-03-16 10:45:09 +0100

    Refactor the oauthclients sql tests (#605)

---
+++ internal/service/oauthclients/model_helper.go
@@ -0,0 +1,59 @@
+package oauthclients
+
+import (
+	"testing"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/theduckcompany/duckcloud/internal/service/users"
+	"github.com/theduckcompany/duckcloud/internal/tools/uuid"
+)
+
+type FakeClientBuilder struct {
+	t      testing.TB
+	client *Client
+}
+
+func NewFakeClient(t testing.TB) *FakeClientBuilder {
+	t.Helper()
+
+	uuidProvider := uuid.NewProvider()
+	createdAt := gofakeit.DateRange(time.Now().Add(-time.Hour*1000), time.Now())
+
+	return &FakeClientBuilder{
+		t: t,
+		client: &Client{
+			id:             uuidProvider.New(),
+			name:           gofakeit.Name(),
+			secret:         gofakeit.Password(true, true, true, false, false, 8),
+			redirectURI:    gofakeit.URL(),
+			userID:         uuidProvider.New(),
+			createdAt:      createdAt,
+			scopes:         Scopes{"scope-a", "scope-b"},
+			public:         false,
+			skipValidation: false,
+		},
+	}
+}
+
+func (f *FakeClientBuilder) CreatedBy(user *users.User) *FakeClientBuilder {
+	f.client.userID = user.ID()
+
+	return f
+}
+
+func (f *FakeClientBuilder) Build() *Client {
+	return f.client
+}
+
+// func (f *FakeClientBuilder) BuildAndStore(ctx context.Context, db *sql.DB) *Client {
+// 	f.t.Helper()
+//
+// 	tools := tools.NewToolboxForTest(f.t)
+// 	storage := newSqlStorage(db, tools)
+//
+// 	err := storage.Save(ctx, f.client)
+// 	require.NoError(f.t, err)
+//
+// 	return f.client
+// }
