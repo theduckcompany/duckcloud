792c4c7a23f10b3800dcdbb978f72349cadd7148
Peltoche <pierre.peltier@protonmail.com>
2024-03-19 17:27:05 +0100

    Make the fx logger less verbose

--- internal/tools/logger/fx.go
+++ internal/tools/logger/fx.go
@@ -30,115 +30,36 @@ func (l *FxLogger) logEvent(msg string, fields ...slog.Attr) {
 // LogEvent logs the given event to the provided Zap logger.
 func (l *FxLogger) LogEvent(event fxevent.Event) {
 	switch e := event.(type) {
-	case *fxevent.OnStartExecuting:
-		l.logEvent("OnStart hook executing",
-			slog.String("callee", e.FunctionName),
-			slog.String("caller", e.CallerName),
-		)
 	case *fxevent.OnStartExecuted:
 		if e.Err != nil {
-			l.logError("OnStart hook failed",
-				slog.String("callee", e.FunctionName),
-				slog.String("caller", e.CallerName),
-				slog.String("error", e.Err.Error()),
-			)
-		} else {
-			l.logEvent("OnStart hook executed",
-				slog.String("callee", e.FunctionName),
-				slog.String("caller", e.CallerName),
-				slog.String("runtime", e.Runtime.String()),
-			)
+			l.logError("OnStart hook failed", slog.String("error", e.Err.Error()))
 		}
-	case *fxevent.OnStopExecuting:
-		l.logEvent("OnStop hook executing",
-			slog.String("callee", e.FunctionName),
-			slog.String("caller", e.CallerName),
-		)
 	case *fxevent.OnStopExecuted:
 		if e.Err != nil {
-			l.logError("OnStop hook failed",
-				slog.String("callee", e.FunctionName),
-				slog.String("caller", e.CallerName),
-				slog.String("error", e.Err.Error()),
-			)
-		} else {
-			l.logEvent("OnStop hook executed",
-				slog.String("callee", e.FunctionName),
-				slog.String("caller", e.CallerName),
-				slog.String("runtime", e.Runtime.String()),
-			)
+			l.logError("OnStop hook failed", slog.String("error", e.Err.Error()))
 		}
 	case *fxevent.Supplied:
 		if e.Err != nil {
-			l.logError("error encountered while applying options",
-				slog.String("type", e.TypeName),
-				moduleName(e.ModuleName),
-				slog.String("error", e.Err.Error()))
-		} else {
-			l.logEvent("supplied",
-				slog.String("type", e.TypeName),
-				moduleName(e.ModuleName),
-			)
+			l.logError("error encountered while applying options", slog.String("error", e.Err.Error()))
 		}
 	case *fxevent.Provided:
-		for _, rtype := range e.OutputTypeNames {
-			l.logEvent("provided",
-				slog.String("constructor", e.ConstructorName),
-				moduleName(e.ModuleName),
-				slog.String("type", rtype),
-			)
-		}
 		if e.Err != nil {
-			l.logError("error encountered while applying options",
-				moduleName(e.ModuleName),
-				slog.String("error", e.Err.Error()),
-			)
+			l.logError("error encountered while applying options", slog.String("error", e.Err.Error()))
 		}
 	case *fxevent.Replaced:
-		for _, rtype := range e.OutputTypeNames {
-			l.logEvent("replaced",
-				moduleName(e.ModuleName),
-				slog.String("type", rtype),
-			)
-		}
 		if e.Err != nil {
-			l.logError("error encountered while replacing",
-				moduleName(e.ModuleName),
-				slog.String("error", e.Err.Error()),
-			)
+			l.logError("error encountered while replacing", slog.String("error", e.Err.Error()))
 		}
 	case *fxevent.Decorated:
-		for _, rtype := range e.OutputTypeNames {
-			l.logEvent("decorated",
-				slog.String("decorator", e.DecoratorName),
-				moduleName(e.ModuleName),
-				slog.String("type", rtype),
-			)
-		}
 		if e.Err != nil {
-			l.logError("error encountered while applying options",
-				moduleName(e.ModuleName),
-				slog.String("error", e.Err.Error()),
-			)
+			l.logError("error encountered while applying options", slog.String("error", e.Err.Error()))
 		}
-	case *fxevent.Invoking:
-		// Do not log stack as it will make logs hard to read.
-		l.logEvent("invoking",
-			slog.String("function", e.FunctionName),
-			moduleName(e.ModuleName),
-		)
 	case *fxevent.Invoked:
 		if e.Err != nil {
-			l.logError("invoke failed",
-				slog.String("error", e.Err.Error()),
-				slog.String("stack", e.Trace),
-				slog.String("function", e.FunctionName),
-				moduleName(e.ModuleName),
-			)
+			l.logError("invoke failed", slog.String("error", e.Err.Error()))
 		}
 	case *fxevent.Stopping:
-		l.logEvent("received signal",
-			slog.String("signal", strings.ToUpper(e.Signal.String())))
+		l.logEvent("received signal", slog.String("signal", strings.ToUpper(e.Signal.String())))
 	case *fxevent.Stopped:
 		if e.Err != nil {
 			l.logError("stop failed", slog.String("error", e.Err.Error()))
@@ -158,15 +79,6 @@ func (l *FxLogger) LogEvent(event fxevent.Event) {
 	case *fxevent.LoggerInitialized:
 		if e.Err != nil {
 			l.logError("custom logger initialization failed", slog.String("error", e.Err.Error()))
-		} else {
-			l.logEvent("initialized custom fxevent.Logger", slog.String("function", e.ConstructorName))
 		}
 	}
-}
-
-func moduleName(name string) slog.Attr {
-	if name == "" {
-		return slog.Attr{}
-	}
-	return slog.String("module", name)
 }
--- internal/tools/logger/fx_test.go
+++ internal/tools/logger/fx_test.go
@@ -8,7 +8,6 @@ import (
 	"log/slog"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -27,30 +26,6 @@ func TestFxLogger(t *testing.T) {
 		wantMessage string
 	}{
 		{
-			name: "OnStartExecuting",
-			give: &fxevent.OnStartExecuting{
-				FunctionName: "hook.onStart",
-				CallerName:   "bytes.NewBuffer",
-			},
-			wantMessage: "OnStart hook executing",
-			wantFields: map[string]interface{}{
-				"caller": "bytes.NewBuffer",
-				"callee": "hook.onStart",
-			},
-		},
-		{
-			name: "OnStopExecuting",
-			give: &fxevent.OnStopExecuting{
-				FunctionName: "hook.onStop1",
-				CallerName:   "bytes.NewBuffer",
-			},
-			wantMessage: "OnStop hook executing",
-			wantFields: map[string]interface{}{
-				"caller": "bytes.NewBuffer",
-				"callee": "hook.onStop1",
-			},
-		},
-		{
 			name: "OnStopExecutedError",
 			give: &fxevent.OnStopExecuted{
 				FunctionName: "hook.onStart1",
@@ -59,23 +34,7 @@ func TestFxLogger(t *testing.T) {
 			},
 			wantMessage: "OnStop hook failed",
 			wantFields: map[string]interface{}{
-				"caller": "bytes.NewBuffer",
-				"callee": "hook.onStart1",
-				"error":  "some error",
-			},
-		},
-		{
-			name: "OnStopExecuted",
-			give: &fxevent.OnStopExecuted{
-				FunctionName: "hook.onStart1",
-				CallerName:   "bytes.NewBuffer",
-				Runtime:      time.Millisecond * 3,
-			},
-			wantMessage: "OnStop hook executed",
-			wantFields: map[string]interface{}{
-				"caller":  "bytes.NewBuffer",
-				"callee":  "hook.onStart1",
-				"runtime": "3ms",
+				"error": "some error",
 			},
 		},
 		{
@@ -87,31 +46,7 @@ func TestFxLogger(t *testing.T) {
 			},
 			wantMessage: "OnStart hook failed",
 			wantFields: map[string]interface{}{
-				"caller": "bytes.NewBuffer",
-				"callee": "hook.onStart1",
-				"error":  "some error",
-			},
-		},
-		{
-			name: "OnStartExecuted",
-			give: &fxevent.OnStartExecuted{
-				FunctionName: "hook.onStart1",
-				CallerName:   "bytes.NewBuffer",
-				Runtime:      time.Millisecond * 3,
-			},
-			wantMessage: "OnStart hook executed",
-			wantFields: map[string]interface{}{
-				"caller":  "bytes.NewBuffer",
-				"callee":  "hook.onStart1",
-				"runtime": "3ms",
-			},
-		},
-		{
-			name:        "Supplied",
-			give:        &fxevent.Supplied{TypeName: "*bytes.Buffer"},
-			wantMessage: "supplied",
-			wantFields: map[string]interface{}{
-				"type": "*bytes.Buffer",
+				"error": "some error",
 			},
 		},
 		{
@@ -119,22 +54,7 @@ func TestFxLogger(t *testing.T) {
 			give:        &fxevent.Supplied{TypeName: "*bytes.Buffer", Err: someError},
 			wantMessage: "error encountered while applying options",
 			wantFields: map[string]interface{}{
-				"type":  "*bytes.Buffer",
 				"error": "some error",
-			},
-		},
-		{
-			name: "Provide",
-			give: &fxevent.Provided{
-				ConstructorName: "bytes.NewBuffer()",
-				ModuleName:      "myModule",
-				OutputTypeNames: []string{"*bytes.Buffer"},
-			},
-			wantMessage: "provided",
-			wantFields: map[string]interface{}{
-				"constructor": "bytes.NewBuffer()",
-				"type":        "*bytes.Buffer",
-				"module":      "myModule",
 			},
 		},
 		{
@@ -143,18 +63,6 @@ func TestFxLogger(t *testing.T) {
 			wantMessage: "error encountered while applying options",
 			wantFields: map[string]interface{}{
 				"error": "some error",
-			},
-		},
-		{
-			name: "Replace",
-			give: &fxevent.Replaced{
-				ModuleName:      "myModule",
-				OutputTypeNames: []string{"*bytes.Buffer"},
-			},
-			wantMessage: "replaced",
-			wantFields: map[string]interface{}{
-				"type":   "*bytes.Buffer",
-				"module": "myModule",
 			},
 		},
 		{
@@ -167,44 +75,11 @@ func TestFxLogger(t *testing.T) {
 			},
 		},
 		{
-			name: "Decorate",
-			give: &fxevent.Decorated{
-				DecoratorName:   "bytes.NewBuffer()",
-				ModuleName:      "myModule",
-				OutputTypeNames: []string{"*bytes.Buffer"},
-			},
-			wantMessage: "decorated",
-			wantFields: map[string]interface{}{
-				"decorator": "bytes.NewBuffer()",
-				"type":      "*bytes.Buffer",
-				"module":    "myModule",
-			},
-		},
-		{
-			name:        "Decorate with Error",
-			give:        &fxevent.Decorated{Err: someError},
-			wantMessage: "error encountered while applying options",
-			wantFields: map[string]interface{}{
-				"error": "some error",
-			},
-		},
-		{
-			name:        "Invoking/Success",
-			give:        &fxevent.Invoking{ModuleName: "myModule", FunctionName: "bytes.NewBuffer()"},
-			wantMessage: "invoking",
-			wantFields: map[string]interface{}{
-				"function": "bytes.NewBuffer()",
-				"module":   "myModule",
-			},
-		},
-		{
 			name:        "Invoked/Error",
-			give:        &fxevent.Invoked{FunctionName: "bytes.NewBuffer()", Err: someError},
+			give:        &fxevent.Invoked{Err: someError},
 			wantMessage: "invoke failed",
 			wantFields: map[string]interface{}{
-				"error":    "some error",
-				"stack":    "",
-				"function": "bytes.NewBuffer()",
+				"error": "some error",
 			},
 		},
 		{
@@ -259,14 +134,6 @@ func TestFxLogger(t *testing.T) {
 			wantMessage: "custom logger initialization failed",
 			wantFields: map[string]interface{}{
 				"error": "some error",
-			},
-		},
-		{
-			name:        "LoggerInitialized",
-			give:        &fxevent.LoggerInitialized{ConstructorName: "bytes.NewBuffer()"},
-			wantMessage: "initialized custom fxevent.Logger",
-			wantFields: map[string]interface{}{
-				"function": "bytes.NewBuffer()",
 			},
 		},
 	}
